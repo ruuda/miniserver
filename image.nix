03f9b81a5fd4951c2cf27a4b704e119b1ca370b4
Ruud van Asseldonk <dev@veniogames.com>
2018-05-07 15:37:01 +0200

    Strip down Nginx even further

--- image.nix
+++ image.nix
@@ -2,31 +2,37 @@
 with pkgs;
 
 let
-  customNginx = nginx.override {
-    # Remove dependency on libgd; It brings in a lot of transitive dependencies
-    # that we don't need (fontconfig, image codecs, etc.).
-    gd = null;
-
-    # Also disable other things we don't need.
-    withStream = false;
-    withMail = false;
-  };
+  customNginx = nginx.overrideDerivation (oldAttrs: {
+    # Override the nginx package to cut down on the dependencies. It does expose
+    # *some* parameters that make a smaller Nginx, but that is not enough for my
+    # taste. I also want to disable geoip and all the xml stuff, I don't use it.
+    configureFlags = [
+      "--with-http_ssl_module"
+      "--with-http_v2_module"
+      "--with-http_gzip_static_module"
+      "--with-threads"
+      "--with-pcre-jit"
+    ];
+  });
 in
   stdenv.mkDerivation {
     name = "miniserver.img";
 
     nativeBuildInputs = [ squashfsTools ];
+    buildInputs = [ customNginx acme-client ];
 
     buildCommand =
       ''
         closureInfo=${closureInfo { rootPaths = [ customNginx acme-client ]; }}
+
+        # Uncomment to print dependencies in the build log.
+        # This is the easiest way I've found to do this.
+        # echo "BEGIN DEPS"
+        # cat $closureInfo/store-paths
+        # echo "END DEPS"
+
         # TODO: Put symlinks binaries in /usr/bin.
-        # TODO: Diagnose the bloat. Why do I have dejavu-fonts-minimal,
-        # libpng, libwebp, freetype, giflib, libtiff, gcc, xz fontconfig?
         # Generate the squashfs image.
-        echo "STORE PATHS"
-        cat $closureInfo/store-paths
-        echo "SQUASH"
         mksquashfs $(cat $closureInfo/store-paths) $out \
           -keep-as-directory -all-root -b 1048576 -comp xz -Xdict-size 100%
       '';
