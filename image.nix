43874d41a475a8d24b9f08c8a3de94e31d3fd5d4
Ruud van Asseldonk <dev@veniogames.com>
2018-05-07 15:05:49 +0200

    Cut down Nginx dependencies
    Build a custom one with less modules. Apparently, .override is the thing
    to use here. This already eliminates a lot of dependencies, but there
    are still more to go. I think I will just pass the configure flags
    myself.

--- image.nix
+++ image.nix
@@ -1,19 +1,33 @@
-{ pkgs ? import <nixpkgs> {}}:
+{pkgs ? import <nixpkgs> {}}:
 with pkgs;
 
-stdenv.mkDerivation {
-  name = "miniserver.img";
+let
+  customNginx = nginx.override {
+    # Remove dependency on libgd; It brings in a lot of transitive dependencies
+    # that we don't need (fontconfig, image codecs, etc.).
+    gd = null;
 
-  nativeBuildInputs = [ squashfsTools ];
+    # Also disable other things we don't need.
+    withStream = false;
+    withMail = false;
+  };
+in
+  stdenv.mkDerivation {
+    name = "miniserver.img";
 
-  buildCommand =
-    ''
-      closureInfo=${closureInfo { rootPaths = [ nginx acme-client ]; }}
-      # TODO: Put symlinks binaries in /usr/bin.
-      # TODO: Diagnose the bloat. Why do I have dejavu-fonts-minimal,
-      # libpng, libwebp, freetype, giflib, libtiff, gcc, xz fontconfig?
-      # Generate the squashfs image.
-      mksquashfs $(cat $closureInfo/store-paths) $out \
-        -keep-as-directory -all-root -b 1048576 -comp xz -Xdict-size 100%
-    '';
-}
+    nativeBuildInputs = [ squashfsTools ];
+
+    buildCommand =
+      ''
+        closureInfo=${closureInfo { rootPaths = [ customNginx acme-client ]; }}
+        # TODO: Put symlinks binaries in /usr/bin.
+        # TODO: Diagnose the bloat. Why do I have dejavu-fonts-minimal,
+        # libpng, libwebp, freetype, giflib, libtiff, gcc, xz fontconfig?
+        # Generate the squashfs image.
+        echo "STORE PATHS"
+        cat $closureInfo/store-paths
+        echo "SQUASH"
+        mksquashfs $(cat $closureInfo/store-paths) $out \
+          -keep-as-directory -all-root -b 1048576 -comp xz -Xdict-size 100%
+      '';
+  }
