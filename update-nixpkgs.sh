9ad58a9655da2142a4d4538944cbe2e1421e9490
Ruud van Asseldonk <dev@veniogames.com>
2018-05-19 00:16:14 +0200

    Add script to update pinned Nixpkgs version

---
+++ update-nixpkgs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Update the pinned Nixpkgs version to the latest commit in the specified
+# channel. Uses the Github API to resolve the channel ref to a commit hash.
+# The pin file contains the hash between quotes, but fortunately jq prints
+# it like that.
+
+channel='nixos-18.03'
+curl --silent "https://api.github.com/repos/NixOS/nixpkgs-channels/git/refs/heads/$channel" | jq .object.sha > nixpkgs-pinned.nix
