ff965940cabf934f5ad9ef13934a71d5696fd675
Ruud van Asseldonk <dev@veniogames.com>
2018-05-20 15:52:54 +0200

    Remove PAM dependency from systemd
    Now the closure is starting to get small.

--- full.nix
+++ full.nix
@@ -4,48 +4,82 @@
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3. A copy
 # of the License is available in the root of the repository.
+let
+  overlay = self: super: {
+    pam = null;
+    utillinuxMinimal = super.utillinuxMinimal.overrideDerivation (oldAttrs: {
+      postInstall = ''
+        rm -fr $out/share/{locale,doc,bash-completion}
+      '';
+    });
+    systemd = super.systemd.override {
+      # Use light versions of dependencies.
+      kbd = super.kbdlight;
 
-{ pkgs ?
-  let
-    # Default to a pinned version of Nixpkgs. The actual revision of the
-    # Nixpkgs repository is stored in a separate file (as a string literal).
-    # We then fetch that revision from Github and import it. The revision
-    # should periodically be updated to be the last commit of NixOS stable.
-    nixpkgsRev = import ./nixpkgs-pinned.nix;
-    pinnedNixpkgs = fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz";
+      # Disable unneeded dependencies.
+      bzip2 = null;
+      kmod = null;
+      libgpgerror = null;
+      libmicrohttpd = null;
+      lz4 = null;
+      pam = null;
+      xz = null;
     };
-  in
-    import pinnedNixpkgs {}
-}:
+  };
+in
+  { pkgs ?
+    let
+      # Default to a pinned version of Nixpkgs. The actual revision of the
+      # Nixpkgs repository is stored in a separate file (as a string literal).
+      # We then fetch that revision from Github and import it. The revision
+      # should periodically be updated to be the last commit of NixOS stable.
+      nixpkgsRev = import ./nixpkgs-pinned.nix;
+      pinnedNixpkgs = fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz";
+      };
+    in
+      import pinnedNixpkgs {
+        overlays = [ overlay ];
+      }
+  }:
 
 with pkgs;
 let
   userland = import ./default.nix { inherit pkgs; };
-  lightKmod = pkgs.kmod.override {
-    xz = null;
-  };
-  customKmod = lightKmod.overrideDerivation (oldAttrs: {
-    configureFlags = lib.remove "--with-xz" oldAttrs.configureFlags;
-  });
-  lightSystemd = pkgs.systemd.override {
-    # withSelinux = false;
-    # withLibseccomp = false;
-    # withKexectools = false;
-    libmicrohttpd = null;
-    libgpgerror = null;
-    xz = null;
-    lz4 = null;
-    bzip2 = null;
-    kmod = customKmod;
-  };
-  disableLz4 = mflag: if mflag == "-Dlz4=true" then "-Dlz4=false" else mflag;
-  customSystemd = lightSystemd.overrideDerivation (oldAttrs: {
-    mesonFlags = map disableLz4 oldAttrs.mesonFlags;
-  });
+  systemd = pkgs.systemd.overrideDerivation (oldAttrs: rec {
+      removeFlags = [
+        # "-Dkmod-path=${null}/bin/kmod"
+        "-Dlz4=true"
+      ];
+      mesonFlags = (lib.foldr lib.remove oldAttrs.mesonFlags removeFlags) ++ [
+        "-Dbzip2=false"
+        "-Dkmod=false"
+        "-Dkmod=false"
+        "-Dlz4=false"
+        "-Dmicrohttpd=false"
+        "-Dpam=false"
+        "-Dxz=false"
+      ];
+      # Adapted from the postinstall in the systemd package.
+      postInstall = ''
+        rm -fr $out/lib/{modules-load.d,binfmt.d,sysctl.d,tmpfiles.d}
+        rm -fr $out/lib/systemd/{system,user}
+        rm -fr $out/etc/systemd/system
+        rm -fr $out/etc/rpm
+        for i in $out/share/dbus-1/system-services/*.service; do
+          substituteInPlace $i --replace /bin/false ${coreutils}/bin/false
+        done
+        find $out -name "*kernel-install*" -exec rm {} \;
+
+        # Keep only libudev and libsystemd in the lib output.
+        mkdir -p $out/lib
+        mv $lib/lib/libnss* $out/lib/
+      '';
+    });
 in {
-  systemd = customSystemd;
-  sshd = openssh;
-  nginx = userland.nginx;
-  acme-client = userland.acme-client;
+  systemd = systemd;
+  utilLinux = pkgs.utillinuxMinimal;
+  # sshd = openssh;
+  # nginx = userland.nginx;
+  # acme-client = userland.acme-client;
 }
