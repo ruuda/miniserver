7b4b723cf18be194bf3c8801d7de5da2f405a2b1
Ruud van Asseldonk <dev@veniogames.com>
2019-03-10 21:25:51 +0100

    Upgrade to latest commit in nixos-unstable channel
    There is a severe, 10% regression in binary size, but diffoscope is not
    helpful, it just says "everything changed". Will need to investigate
    later.

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/36f316007494c388df1fec434c1e658542e3c3cc.tar.gz";
-  sha256 = "1w1dg9ankgi59r2mh0jilccz5c4gv30a6q1k6kv2sn8vfjazwp9k";
+  url = "https://github.com/NixOS/nixpkgs/archive/5d3fd3674a66c5b1ada63e2eace140519849c967.tar.gz";
+  sha256 = "1yjn56jsczih4chjcll63a20v3nwv1jhl2rf6rk8d8cwvb10g0mk";
 }
