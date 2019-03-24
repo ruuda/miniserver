17e419f642e19644dfd48879c7274ee344fe6665
Ruud van Asseldonk <dev@veniogames.com>
2019-03-24 17:56:50 +0100

    Upgrade to latest commit in nixos-unstable channel

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/5d3fd3674a66c5b1ada63e2eace140519849c967.tar.gz";
-  sha256 = "1yjn56jsczih4chjcll63a20v3nwv1jhl2rf6rk8d8cwvb10g0mk";
+  url = "https://github.com/NixOS/nixpkgs/archive/373488e6f4c3dc3bb51cabcb959e4a70eb5d7b2c.tar.gz";
+  sha256 = "0zivy6478shphlb7xx2hspf696mh9rlazyprcl0r1ksibxsz5y8s";
 }
