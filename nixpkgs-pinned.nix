69471983c357df163cd4e24668f0a1730a54d710
Ruud van Asseldonk <dev@veniogames.com>
2019-05-08 21:34:08 +0200

    Upgrade to nixpkgs with LibreSSL 2.9.1
    This includes nixpkgs/27fd944bb73f70377379a8a59ac3a88fd32e26bf, which
    updates LibreSSL from 2.9.0 to 2.9.1.

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/190727db4ea7e0d083e7dbcb66ced11f31b340f0.tar.gz";
-  sha256 = "0s2wd2khcd6iynm7ilc9f3cadj0mfyk5q2bik8zi66ljv9rm2di9";
+  url = "https://github.com/NixOS/nixpkgs/archive/27fd944bb73f70377379a8a59ac3a88fd32e26bf.tar.gz";
+  sha256 = "15lddqq5nh150izdc2l8n9c7ncfcmgnwqsrg6229r6vxy7lk9lwg";
 }
