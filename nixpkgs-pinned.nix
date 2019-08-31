ea7af73a5918893e5ad474e8468610c7158f5d89
Ruud van Asseldonk <dev@veniogames.com>
2019-08-31 16:45:53 +0200

    Upgrade to latest commit in nixos-unstable channel
    This commit includes my fixes to build LibreSSL without executable
    stack, and also LibreSSL 3.0.

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/ruuda/nixpkgs/archive/d30e42cbd4f42035d198fbd3f5210a83e34738b2.tar.gz";
-  sha256 = "0z5pq95nsxgg29bv440c886sxgqnjk7cgqprr9drinbgdqsyx51n";
+  url = "https://github.com/NixOS/nixpkgs/archive/8d1510abfb592339e13ce8f6db6f29c1f8b72924.tar.gz";
+  sha256 = "0i4jscq2qy2vn9kr8ix9gr8ncf9dss24y09jrqhlpcpryi4i98dc";
 }
