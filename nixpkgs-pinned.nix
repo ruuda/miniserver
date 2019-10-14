7043a89a9ae47cdebd1bfb05e12d802138b1f64d
Ruud van Asseldonk <dev@veniogames.com>
2019-10-14 21:16:03 +0200

    Upgrade to latest commit in nixos-unstable channel
    I expected this to include LibreSSL 3.0.1, which includes a fix for
    CVE-2019-1547 and a fixed memory leak. However, it seems that change
    hasn't made it into nixos-unstable just yet.

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/2436c27541b2f52deea3a4c1691216a02152e729.tar.gz";
-  sha256 = "0p98dwy3rbvdp6np596sfqnwlra11pif3rbdh02pwdyjmdvkmbvd";
+  url = "https://github.com/NixOS/nixpkgs/archive/94500c93dc239761bd144128a1684abcd08df6b7.tar.gz";
+  sha256 = "0p6bd16mb1k5c3zihl1c3d62m419rii102xfb340pccgbd8j34bc";
 }
