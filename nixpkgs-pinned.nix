ec06e9fd364f55903551a5a79c82732dc2a34ebd
Ruud van Asseldonk <dev@veniogames.com>
2019-10-23 19:12:41 +0200

    Upgrade to Nixpkgs with LibreSSL 3.0.2
    This is taken from my own fork of Nixpkgs, I have opened a pull request
    to bump the version upstream, but meanwhile, we can already use it here.

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/67effde499ffb60da484ebbbd643c6dc69f8eb77.tar.gz";
-  sha256 = "1fvnajj5l9zrvv2k55i6mcmjs8ymc8py8ygcz8kg3lghm15drrdd";
+  url = "https://github.com/NixOS/nixpkgs/archive/1126db828f40e8e583ef1a12a37fd65503b76329.tar.gz";
+  sha256 = "0i3h477arl8qni7afxnwkkzpm5ikagrdh1mh5hnxw1a12l2xvf25";
 }
