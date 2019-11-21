025b1d8d7e465d2357a5558dae413fadc4749b53
Ruud van Asseldonk <dev@veniogames.com>
2019-11-21 20:47:36 +0100

    Upgrade to latest commit in nixos-unstable channel
    acme-client 0.1.16 -> 0.2.4
    nss-cacert  3.46   -> 3.46.1

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/1126db828f40e8e583ef1a12a37fd65503b76329.tar.gz";
-  sha256 = "0i3h477arl8qni7afxnwkkzpm5ikagrdh1mh5hnxw1a12l2xvf25";
+  url = "https://github.com/NixOS/nixpkgs/archive/e89b21504f3e61e535229afa0b121defb52d2a50.tar.gz";
+  sha256 = "0jqcv3rfki3mwda00g66d27k6q2y7ca5mslrnshfpbdm7j8ya0kj";
 }
