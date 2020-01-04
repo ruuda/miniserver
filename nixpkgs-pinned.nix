ff417fa5f522f9a8e024e8011d0c82e13582c07a
Ruud van Asseldonk <dev@veniogames.com>
2020-01-04 22:27:13 +0100

    Upgrade to latest commit in nixos-unstable channel
      acme-client 0.2.4 -> 0.2.5

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/f5bb6c020718c56ffe691f1fa187a9d8bbec4520.tar.gz";
-  sha256 = "09gig1zpijvm7dbp0ykw9gza7nkjxiaj4jb3p0dff8fmjyyrq78b";
+  url = "https://github.com/NixOS/nixpkgs/archive/e0470e11c7a02f9e6e70f5ec5e1d9470c742b396.tar.gz";
+  sha256 = "1amczhr8m7lvxnxzwhfamz4ga78sgnyzdfr759iq26azkh6fa03a";
 }
