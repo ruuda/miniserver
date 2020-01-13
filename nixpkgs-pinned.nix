ee5ec4c4e8b2135680d9b8c8a11c447faa8bf25a
Ruud van Asseldonk <dev@veniogames.com>
2020-01-13 08:46:48 +0100

    Upgrade to latest commit in nixos-unstable channel
      nss-cacert 3.47.1 -> 3.48

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/e0470e11c7a02f9e6e70f5ec5e1d9470c742b396.tar.gz";
-  sha256 = "1amczhr8m7lvxnxzwhfamz4ga78sgnyzdfr759iq26azkh6fa03a";
+  url = "https://github.com/NixOS/nixpkgs/archive/100012e55bc2a82fc680cba31a426ad38ead6fab.tar.gz";
+  sha256 = "0cvnx852n0krci9hi1rpcldx1kcpmvi5ihf2awvwfayvzp4wic8z";
 }
