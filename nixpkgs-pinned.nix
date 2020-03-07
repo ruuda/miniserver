98c073cebe93ed20bc9a51ea86f72432f433ea35
Ruud van Asseldonk <dev@veniogames.com>
2020-03-07 14:14:21 +0100

    Upgrade to latest commit in nixos-unstable channel
      pcre 8.43 -> 8.44

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/e2b4abe3c8f2e09adfc6a52007841d1b96c89371.tar.gz";
-  sha256 = "1l3jfr74s7wmx3fk5y76ayazbfclvnr532kg1hypbzydp3n372rz";
+  url = "https://github.com/NixOS/nixpkgs/archive/82b54d490663b6d87b7b34b9cfc0985df8b49c7d.tar.gz";
+  sha256 = "12gpsif48g5b4ys45x36g4vdf0srgal4c96351m7gd2jsgvdllyf";
 }
