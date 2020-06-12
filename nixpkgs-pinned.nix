ac4d66e82c2a21175b5791483aad63d659a4c310
Ruud van Asseldonk <dev@veniogames.com>
2020-06-12 21:16:50 +0200

    Upgrade to latest commit in nixos-unstable channel
      nginx 1.18.0 -> 1.19.0

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/8ba41a1e14961fe43523f29b8b39acb569b70e72.tar.gz";
-  sha256 = "0c2wn7si8vcx0yqwm92dpry8zqjglj9dfrvmww6ha6ihnjl6mfhh";
+  url = "https://github.com/NixOS/nixpkgs-channels/archive/029a5de08390bb03c3f44230b064fd1850c6658a.tar.gz";
+  sha256 = "03fjkzhrs2avcvdabgm7a65rnyjaqbqdnv4q86qyjkkwg64g5m8x";
 })
