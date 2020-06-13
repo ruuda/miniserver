54d1bdb1c66d2149e44f34c28b39ab4be00b8134
Ruud van Asseldonk <dev@veniogames.com>
2020-06-13 21:17:35 +0200

    Remove unused file

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -i bash -p diffoscope
-if ! nix-build --option build-repeat 5 -K; then
-  path=$(nix-store -q $(nix-instantiate))
-  diffoscope "${path}" "${path}.check"
-fi
-
