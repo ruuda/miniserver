f9ed0ed184d3b23b593fc2486f2820ea36e7d417
Ruud van Asseldonk <dev@veniogames.com>
2020-06-19 21:50:32 +0200

    Update libressl to 3.1.3, and one more change
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    master branch of nixos/nixpkgs.
    
      libressl   3.0.2  -> 3.1.3
    - nss-cacert 3.52.1

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/51b989a513c51e6f355ae85037fa160173b6d44c.tar.gz";
-  sha256 = "0dxj9smpvkmr79nakv64qkqbk1zzih0lwgmlwfw71k1nn4yrzlc4";
+  url = "https://github.com/nixos/nixpkgs/archive/619f370777e88b8ea038e9f7389f2fd2b9e31661.tar.gz";
+  sha256 = "0qsf0s5as35n1jqlwbay1f4d6bylca0596zgkv0pgm74q3jmyj1h";
 })
