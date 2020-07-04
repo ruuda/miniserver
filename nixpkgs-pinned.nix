23ab91f4d060f45a307fd5fa65de00d076e298cc
Ruud van Asseldonk <dev@veniogames.com>
2020-07-04 16:22:50 +0200

    Update systemd, libksba, cmake, bison, file
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs-channels.
    
    Build dependencies:
    
      bison   3.6.2  -> 3.6.3
      cmake   3.17.2 -> 3.17.3
      file    5.38   -> 5.39
      libksba 1.3.5  -> 1.4.0
      systemd 245.5  -> 245.6

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/619f370777e88b8ea038e9f7389f2fd2b9e31661.tar.gz";
-  sha256 = "0qsf0s5as35n1jqlwbay1f4d6bylca0596zgkv0pgm74q3jmyj1h";
+  url = "https://github.com/nixos/nixpkgs-channels/archive/55668eb671b915b49bcaaeec4518cc49d8de0a99.tar.gz";
+  sha256 = "0b2mrrjsdpxpyvnv98dph5av3xjps1mbd87x8510mnc4pfa2zc8z";
 })
