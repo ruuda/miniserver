0df77a1b3f3bd19e7208c18a4c48c6147a52d650
Ruud van Asseldonk <dev@veniogames.com>
2020-08-23 17:41:57 +0200

    Update wheel, meson, gettext, mpfr
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs-channels.
    
    Build dependencies:
    
      gettext 0.20.1 -> 0.21
      meson   0.54.2 -> 0.55.0
      mpfr    4.0.2  -> 4.1.0
      wheel   0.33.6 -> 0.34.2

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs-channels/archive/8e2b14aceb1d40c7e8b84c03a7c78955359872bb.tar.gz";
-  sha256 = "0zzjpd9smr7rxzrdf6raw9kbj42fbvafxb5bz36lcxgv290pgsm8";
+  url = "https://github.com/nixos/nixpkgs-channels/archive/c59ea8b8a0e7f927e7291c14ea6cd1bd3a16ff38.tar.gz";
+  sha256 = "1ak7jqx94fjhc68xh1lh35kh3w3ndbadprrb762qgvcfb8351x8v";
 })
