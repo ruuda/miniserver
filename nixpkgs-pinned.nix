291ed084a718b0ad20e9ee00059d296594ec25bc
Ruud van Asseldonk <dev@veniogames.com>
2021-02-08 21:04:00 +0100

    Update acme-client, glibc, and 14 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      acme-client 1.0.1   -> 1.1.0
      glibc       2.32-25 -> 2.32-35
    
    Build dependencies:
    
      asciidoc       8.6.9   -> 9.0.4
      cmake          3.19.2  -> 3.19.3
      cmake-boot     3.19.2  -> 3.19.3
    - db             5.3.28
    + docbook-xsl-ns            1.79.2
      ed             1.16    -> 1.17
      glibc-iconv    2.32-25 -> 2.32-35
      glibc-locales  2.32-25 -> 2.32-35
      libarchive     3.5.0   -> 3.5.1
      libcap         2.44    -> 2.46
      linux-headers  5.10.4  -> 5.10.9
      lzip           1.21    -> 1.22
    - python         2.7.18
      sqlite         3.34.0  -> 3.34.1

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/f217c0ea7c148ddc0103347051555c7c252dcafb.tar.gz";
-  sha256 = "0cyksxg2lnzxd0pss09rmmk2c2axz0lf9wvgvfng59nwf8dpq2kf";
+  url = "https://github.com/nixos/nixpkgs/archive/536fe36e23ab0fc8b7f35c24603422eee9fc17a2.tar.gz";
+  sha256 = "040j2bv45vsa106gblphzgjx2zq90c8mh2iy1ig9xly7xsw15681";
 })
