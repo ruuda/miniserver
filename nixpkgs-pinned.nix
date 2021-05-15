33a1b0ce9c02f58191a6e7f8af85af7509266428
Ruud van Asseldonk <dev@veniogames.com>
2021-05-15 14:26:06 +0200

    Update nginx to 1.20.0, and 3 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      nginx   1.19.9 -> 1.20.0
    - zlib    1.2.11
    + zlib-ng           2.0.2
    
    Build dependencies:
    
    + zlib      1.2.11

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/8e4fe32876ca15e3d5eb3ecd3ca0b224417f5f17.tar.gz";
-  sha256 = "1l7bnn2mlwmbi6s9kqa1g2i66arzshqijym1qmqq5417q5pq1da7";
+  url = "https://github.com/nixos/nixpkgs/archive/65a9923bbb94e4da656eb63f88f3a378be232e02.tar.gz";
+  sha256 = "1hwrdhb6izis0zqqxriqm8gsqylj0awf0fn72xd3l2iqvhkhl8p2";
 })
