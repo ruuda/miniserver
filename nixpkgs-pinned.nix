03f8ecc4858641d93c2dd8973b631a969714b727
Ruud van Asseldonk <dev@veniogames.com>
2021-05-25 21:23:55 +0200

    Update libidn2, glibc, and 17 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      glibc   2.32-40 -> 2.32-46
      libidn2 2.3.0   -> 2.3.1
    
    Build dependencies:
    
      autoconf                     2.70    -> 2.71
      bootstrap-stage4-gcc-wrapper 10.2.0  -> 10.3.0
      curl                         7.74.0  -> 7.76.1
      gcc                          10.2.0  -> 10.3.0
      gcc-wrapper                  10.2.0  -> 10.3.0
      gdb                          10.1    -> 10.2
      glibc-iconv                  2.32-40 -> 2.32
      glibc-locales                2.32-40 -> 2.32-46
    - hidapi                       0.10.1
      kbd                          2.0.4   -> 2.4.0
      libfido2                     1.5.0   -> 1.7.0
    - libusb                       1.0.24
      libxml2                      2.9.10  -> 2.9.12
      linux-headers                5.11    -> 5.12
      openssh                      8.5p1   -> 8.6p1
      re2c                         1.3     -> 2.1.1
      readline                     8.0p4   -> 8.1p0

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/65a9923bbb94e4da656eb63f88f3a378be232e02.tar.gz";
-  sha256 = "1hwrdhb6izis0zqqxriqm8gsqylj0awf0fn72xd3l2iqvhkhl8p2";
+  url = "https://github.com/nixos/nixpkgs/archive/ea7d4aa9b8225abd6147339f0d56675d6f1f0fd1.tar.gz";
+  sha256 = "11bwgyglag40w2y50nq2gg4697ymdjprkgip75kb3hjrvvwpma53";
 })
