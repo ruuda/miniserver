57992c203253b85eb55bd9192d5a28524340ff5e
Ruud van Asseldonk <dev@veniogames.com>
2021-08-07 12:00:29 +0200

    Update python3-minimal, setuptools, and 13 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Build dependencies:
    
      attr             2.4.48      -> 2.5.1
    + boost                           1.69.0
      file             5.39        -> 5.40
      findutils        4.7.0       -> 4.8.0
    - getent           1003.1-2008
    + getent-glibc                    2.33-47
    + icu4c                           69.1
      libcap           2.48        -> 2.49
      libgpg-error     1.41        -> 1.42
      libuv            1.41.0      -> 1.42.0
      pip              21.0.1      -> 21.1.3
      python3          3.9.5       -> 3.9.6
      python3-minimal  3.9.5       -> 3.9.6
      setuptools       54.2.0      -> 57.2.0
    + source-highlight                3.1.9

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/8ecc61c91a596df7d3293603a9c2384190c1b89a.tar.gz";
-  sha256 = "0vhajylsmipjkm5v44n2h0pglcmpvk4mkyvxp7qfvkjdxw21dyml";
+  url = "https://github.com/nixos/nixpkgs/archive/439b1605227b8adb1357b55ce8529d541abbe9eb.tar.gz";
+  sha256 = "1jmlia9670lq89ic241zvif5q68qsqhqzm68pbdacxjmdrr0ahqf";
 })
