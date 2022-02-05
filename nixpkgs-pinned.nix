2095729d3f85aac2320b4592be80bade9f7f55e5
Ruud van Asseldonk <dev@veniogames.com>
2022-02-05 19:54:27 +0100

    Update glibc, glibc-locales, and 2 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      glibc 2.33-62 -> 2.33-108
    
    Build dependencies:
    
      getent-glibc  2.33-62 -> 2.33-108
      glibc-locales 2.33-62 -> 2.33-108
      util-linux    2.37.2  -> 2.37.3

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/5bb20f9dc70e9ee16e21cc404b6508654931ce41.tar.gz";
-  sha256 = "0h92fdz44y12cqx2c3qnccik4zlzp55xb4ywgdlr99pzbxf74jrv";
+  url = "https://github.com/nixos/nixpkgs/archive/554d2d8aa25b6e583575459c297ec23750adb6cb.tar.gz";
+  sha256 = "01yfqslnkyrfb5yjfablhvw830iw0za3mab4n03a0ldyq5ac6wh1";
 })
