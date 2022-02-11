2206d3ce1718e64956ef6ec2d252aca2433b0e6d
Ruud van Asseldonk <dev@veniogames.com>
2022-02-11 22:01:45 +0100

    Move image deployment code into function

--- miniserver.py
+++ miniserver.py
@@ -26,6 +26,7 @@ from typing import Dict, Iterator
 
 from nix_store import NIX_BIN, ensure_pinned_nix_version, run
 
+
 def get_current_image_path() -> str:
     ensure_pinned_nix_version()
     return run(f'{NIX_BIN}/nix', 'path-info').rstrip('\n')
@@ -77,6 +78,54 @@ def sshfs(host: str) -> Iterator[str]:
     os.rmdir(tmp_path)
 
 
+def deploy_image(
+    release_name: str,
+    release_path: str,
+    tmp_path: str,
+) -> None:
+    target_dir = f'{tmp_path}/store/{release_name}'
+    now = datetime.now(timezone.utc)
+
+    os.makedirs(target_dir, exist_ok=True)
+    shutil.copyfile(release_path, f'{target_dir}/miniserver.img')
+    copy_replace_file(
+        'nginx.service',
+        f'{target_dir}/nginx.service',
+        {'{{ROOT_IMAGE}}': f'/var/lib/miniserver/store/{release_name}/miniserver.img'},
+    )
+    copy_replace_file(
+        'acme-client.service',
+        f'{target_dir}/acme-client.service',
+        {'{{ROOT_IMAGE}}': f'/var/lib/miniserver/store/{release_name}/miniserver.img'},
+    )
+
+    # Record when we deployed this version.
+    with open(f'{tmp_path}/deploy.log', 'a', encoding='utf-8') as deploylog:
+        deploylog.write(f'{now.isoformat()} {release_name}\n')
+
+    try:
+        before_link = os.readlink(f'{tmp_path}/current')
+        if before_link == f'store/{release_name}':
+            print('Re-deploying, not updating "previous" link.')
+            os.remove(f'{tmp_path}/current')
+        else:
+            print(f'Linked "previous" -> "{before_link}".')
+            os.replace(f'{tmp_path}/current', f'{tmp_path}/previous')
+
+    except FileNotFoundError:
+        print('No current deployment found, not creating a "previous" link.')
+        pass
+
+    os.symlink(
+        src=f'store/{release_name}',
+        dst=f'{tmp_path}/current',
+        target_is_directory=True,
+    )
+    print(f'Linked "current" -> "store/{release_name}".')
+
+    # TODO: Delete old releases.
+
+
 def main() -> None:
     args = sys.argv[1:]
 
@@ -102,47 +151,7 @@ def main() -> None:
     print('Deploying', release_path, '...')
 
     with sshfs(host) as tmp_path:
-        target_dir = f'{tmp_path}/store/{release_name}'
-        now = datetime.now(timezone.utc)
-
-        os.makedirs(target_dir, exist_ok=True)
-        shutil.copyfile(release_path, f'{target_dir}/miniserver.img')
-        copy_replace_file(
-            'nginx.service',
-            f'{target_dir}/nginx.service',
-            {'{{ROOT_IMAGE}}': f'/var/lib/miniserver/store/{release_name}/miniserver.img'},
-        )
-        copy_replace_file(
-            'acme-client.service',
-            f'{target_dir}/acme-client.service',
-            {'{{ROOT_IMAGE}}': f'/var/lib/miniserver/store/{release_name}/miniserver.img'},
-        )
-
-        # Record when we deployed this version.
-        with open(f'{tmp_path}/deploy.log', 'a', encoding='utf-8') as deploylog:
-            deploylog.write(f'{now.isoformat()} {release_name}\n')
-
-        try:
-            before_link = os.readlink(f'{tmp_path}/current')
-            if before_link == f'store/{release_name}':
-                print('Re-deploying, not updating "previous" link.')
-                os.remove(f'{tmp_path}/current')
-            else:
-                print(f'Linked "previous" -> "{before_link}".')
-                os.replace(f'{tmp_path}/current', f'{tmp_path}/previous')
-
-        except FileNotFoundError:
-            print('No current deployment found, not creating a "previous" link.')
-            pass
-
-        os.symlink(
-            src=f'store/{release_name}',
-            dst=f'{tmp_path}/current',
-            target_is_directory=True,
-        )
-        print(f'Linked "current" -> "store/{release_name}".')
-
-        # TODO: Delete old releases.
+        deploy_image(release_name, release_path, tmp_path)
 
 
 if __name__ == '__main__':
