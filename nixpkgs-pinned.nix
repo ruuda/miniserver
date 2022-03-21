b80cca32b1cc2828e44a2a7ca84fd17b4b7ebf39
Ruud van Asseldonk <dev@veniogames.com>
2022-03-21 22:00:36 +0100

    Update libressl, glibc, and 19 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      glibc    2.33-108 -> 2.33-117
      libressl 3.4.2    -> 3.4.3
    
    Build dependencies:
    
      bash             5.1-p12  -> 5.1-p16
      bash-interactive 5.1-p12  -> 5.1-p16
      ed               1.17     -> 1.18
      expat            2.4.4    -> 2.4.6
      findutils        4.8.0    -> 4.9.0
      gdbm             1.20     -> 1.23
      getent-glibc     2.33-108 -> 2.33-117
      glibc-locales    2.33-108 -> 2.33-117
      gnupg            2.3.3    -> 2.3.4
      help2man         1.48.5   -> 1.49.1
      libarchive       3.5.2    -> 3.6.0
      libbsd           0.11.3   -> 0.11.5
      libxml2          2.9.12   -> 2.9.13
      libxslt          1.1.34   -> 1.1.35
      lvm2             2.03.14  -> 2.03.15
      openssl          1.1.1m   -> 1.1.1n
      readline         8.1p0    -> 8.1p2
    + swig                         3.0.12
      util-linux       2.37.3   -> 2.37.4

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/fcd48a5a0693f016a5c370460d0c2a8243b882dc.tar.gz";
-  sha256 = "06nyvac1azpa9gqdhj9py6ljsvbfgx49ilp8kf6w0w9clxba64vg";
+  url = "https://github.com/nixos/nixpkgs/archive/9bc841fec1c0e8b9772afa29f934d2c7ce57da8e.tar.gz";
+  sha256 = "0hh1gpw3lcacz3qba7pcc7j1vc09gawa4lzff3mkgsrmp732hg5f";
 })
