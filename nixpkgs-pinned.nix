795d4a12dc91efc0f7e06a35b89287a8e460630d
Ruud van Asseldonk <dev@veniogames.com>
2022-03-24 20:56:12 +0100

    Update squashfs to 4.5.1
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Build dependencies:
    
      squashfs 4.5 -> 4.5.1

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/9bc841fec1c0e8b9772afa29f934d2c7ce57da8e.tar.gz";
-  sha256 = "0hh1gpw3lcacz3qba7pcc7j1vc09gawa4lzff3mkgsrmp732hg5f";
+  url = "https://github.com/nixos/nixpkgs/archive/1ec61dd4167f04be8d05c45780818826132eea0d.tar.gz";
+  sha256 = "0aglyrxqkfwm4wxlz642vcgn0m350jv4nhhyq91cxylvs1avps54";
 })
