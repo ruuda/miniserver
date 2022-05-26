ad1f57762a30f34077b85986b0f1ac7eda791f8d
Ruud van Asseldonk <dev@veniogames.com>
2022-05-26 12:45:21 +0200

    Update libunistring, glibc, and 70 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      glibc        2.34-115 -> 2.34-210
      libunistring 0.9.10   -> 1.0
    
    Build dependencies:
    
    - Babel                        2.9.1
      Jinja2                       3.0.3        -> 3.1.2
    - Pygments                     2.11.2
    - atomicwrites                 1.4.0
    + babel                                        2.10.1
      binutils                     2.35.2       -> 2.38
      binutils-wrapper             2.35.2       -> 2.38
      bootstrap-stage4-gcc-wrapper 10.3.0       -> 11.3.0
    + cargo                                        1.60.0
    + cargo-bootstrap                              1.59.0
    - coverage                     6.3.2
      curl                         7.82.0       -> 7.83.1
      docutils                     0.17.1       -> 0.18.1
      expat                        2.4.7        -> 2.4.8
    + fb-re2                                       1.0.7
      flit-core                    3.6.0        -> 3.7.1
      freezegun                    1.1.0        -> 1.2.1
    + fuse                                         2.9.9
    + fuse                                         3.10.5
      gcc                          10.3.0       -> 11.3.0
      gcc-wrapper                  10.3.0       -> 11.3.0
      getent-glibc                 2.34-115     -> 2.34-210
      git-minimal                  2.35.1       -> 2.36.0
      glibc-locales                2.34-115     -> 2.34-210
    + gnu-config                                   2021-01-25
    - hostname-net-tools           2.10
      hypothesis                   6.38.0       -> 6.40.0
      icu4c                        70.1         -> 71.1
      libarchive                   3.6.0        -> 3.6.1
    + libbfd                                       2.38
      libbsd                       0.11.5       -> 0.11.6
      libcap-ng                    0.8.2        -> 0.8.3
    - libcbor                      0.9.0
    - libedit                      20210910-3.1
    - libfido2                     1.10.0
    + libiberty                                    11.3.0
      libkrb5                      1.19.2       -> 1.19.3
    + libpfm                                       4.11.0
    - libredirect                  0
      libtool                      2.4.6        -> 2.4.7
      libxml2                      2.9.13       -> 2.9.14
      linux-headers                5.16         -> 5.17
    + llvm                                         14.0.1
    + llvm-src                                     14.0.1
      lzip                         1.22         -> 1.23
      markupsafe                   2.1.0        -> 2.1.1
    + mercurial                                    6.1.2
    - more-itertools               8.12.0
    - net-tools                    2.10
    + nginx-doc-unstable                           2022-05-05
    - nose                         1.3.7
      nss-cacert                   3.74         -> 3.77
      nss-cacert-certdata          3.74         -> 3.77
    - openssh                      9.0p1
      openssl                      1.1.1n       -> 1.1.1o
      perl                         5.34.0       -> 5.34.1
      pip                          22.0.3       -> 22.0.4
    + pygments                                     2.12.0
      pyparsing                    3.0.7        -> 3.0.9
      pytest                       7.0.1        -> 7.1.1
      python3                      3.9.11       -> 3.9.12
      python3-minimal              3.9.11       -> 3.9.12
      pytz                         2021.3       -> 2022.1
    + re2                                          2022-04-01
    + rustc                                        1.60.0
    + rustc-bootstrap                              1.59.0
      setuptools                   60.8.2       -> 61.2.0
      sqlite                       3.38.2       -> 3.38.5
    + toml                                         0.10.2
    - wcwidth                      0.2.5

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/1ffba9f2f683063c2b14c9f4d12c55ad5f4ed887.tar.gz";
-  sha256 = "1zx5zvpvqrgk5mfxzmwf8gd270lz7dkfk563sccp1xlhac15cipg";
+  url = "https://github.com/nixos/nixpkgs/archive/5ce6597eca7d7b518c03ecda57d45f9404b5e060.tar.gz";
+  sha256 = "1hs1lnnbp1dky3nfp7xlricpp5c63sr46jyrnvykci8bl8jnxnl3";
 })
