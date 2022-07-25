2f3619f0de7df24550e250c549dba77287e76d90
Ruud van Asseldonk <dev@veniogames.com>
2022-07-25 20:46:59 +0200

    Update libressl, autoconf-archive, and 47 changes
    This updates the pinned Nixpkgs snapshot to commit
    4c589a926132de94a5d9889ac306378985776d2c of ruuda/nixpkgs.
    
    Runtime dependencies:
    
      libressl 3.4.3 -> 3.5.3
    
    Build dependencies:
    
    - Cython              0.29.28
    + apipkg                              2.1.1
      asciidoc            9.1.0        -> 10.2.0
      autoconf-archive    2021.02.19   -> 2022.02.11
      babel               2.10.1       -> 2.10.3
    - boost               1.79.0
    - boost-build         boost-1.79.0
      cargo               1.61.0       -> 1.62.0
      cargo-bootstrap     1.60.0       -> 1.61.0
      cmake               3.22.3       -> 3.23.2
      cmake-boot          3.22.3       -> 3.23.2
    + coverage                            6.3.2
      curl                7.83.1       -> 7.84.0
    + cython                              0.29.30
    + execnet                             1.9.0
    + filelock                            3.7.1
    + flaky                               3.7.0
      git-minimal         2.36.1       -> 2.37.0
    + gmp-with-cxx-stage3                 6.2.1
    + gmp-with-cxx-stage4                 6.2.1
    - icu4c               71.1
    - isl                 0.20
    + isl-stage3                          0.20
      libkrb5             1.19.3       -> 1.20
    - libmpc              1.2.1
    + libmpc-stage3                       1.2.1
      llvm                14.0.1       -> 14.0.6
      llvm-src            14.0.1       -> 14.0.6
      mercurial           6.1.4        -> 6.2
    + mock                                4.0.3
    + mpfr-stage3                         4.1.0
    + nose                                1.3.7
      nss-cacert          3.77         -> 3.80
      nss-cacert-certdata 3.77         -> 3.80
      openssl             1.1.1p       -> 1.1.1q
    + pexpect                             4.8.0
    + psutil                              5.9.1
    + ptyprocess                          0.7.0
    + pytest-asyncio                      0.18.3
    + pytest-forked                       1.4.0
    + pytest-mock                         3.8.1
    + pytest-xdist                        2.5.0
      re2                 2022-04-01   -> 2022-06-01
      rustc               1.61.0       -> 1.62.0
      rustc-bootstrap     1.60.0       -> 1.61.0
    - source-highlight    3.1.9
      sqlite              3.38.5       -> 3.39.0
      util-linux-minimal  2.37.4       -> 2.38

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/71a4f0dc3d80ba76f437c888c1c3d59f1df98163.tar.gz";
-  sha256 = "0vf3417mqwnpl7j98gig7ig4n0h59gih43qsvgrwnfcw85f62kmi";
+  url = "https://github.com/ruuda/nixpkgs/archive/4c589a926132de94a5d9889ac306378985776d2c.tar.gz";
+  sha256 = "0jhzl1536wjr84x9x21wv00qxibb03igifgm9a11wxfpnd8bqaiv";
 })
