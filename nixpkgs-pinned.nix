2b739b95108a096561c172c7d585716866f10b90
Ruud van Asseldonk <dev@veniogames.com>
2022-10-14 21:01:54 +0200

    Update iana-etc, libressl, tzdata, and 40 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      iana-etc 20220520 -> 20220915
      lego     4.8.0    -> 4.9.0
      libressl 3.5.3    -> 3.6.0
      tzdata   2022c    -> 2022d
    
    Build dependencies:
    
      binutils            2.38              -> 2.39
      binutils-wrapper    2.38              -> 2.39
      boehm-gc            8.0.6             -> 8.2.2
      bundler             2.3.20            -> 2.3.22
      cargo               1.63.0            -> 1.64.0
      cargo-bootstrap     1.62.1            -> 1.63.0
      cmake               3.24.1            -> 3.24.2
      cmake-boot          3.24.1            -> 3.24.2
      curl                7.84.0            -> 7.85.0
      cython              0.29.30           -> 0.29.32
      expat               2.4.8             -> 2.4.9
      file                5.42              -> 5.43
      freezegun           1.2.1             -> 1.2.2
      git                 2.37.2            -> 2.37.3
      git-minimal         2.37.2            -> 2.37.3
      go                  1.18.5            -> 1.18.6
      kbd                 2.4.0             -> 2.5.1
      libbfd              2.38              -> 2.39
      libcap              2.65              -> 2.66
      libffi              3.4.2             -> 3.4.3
      libpfm              4.11.0            -> 4.12.0
      libpipeline         1.5.4             -> 1.5.6
      libxml2             2.9.14            -> 2.10.0
      libxslt             1.1.35            -> 1.1.36
      mercurial           6.2.2             -> 6.2.3
      nghttp2             1.47.0            -> 1.49.0
      ninja               1.11.0            -> 1.11.1
      nss-cacert          3.80              -> 3.83
      nss-cacert-certdata 3.80              -> 3.83
      pip                 22.1.2            -> 22.2.2
      pytest              7.1.2             -> 7.1.3
      python3             3.10.6            -> 3.10.7
      python3-minimal     3.10.6            -> 3.10.7
      pytz                2022.1            -> 2022.2.1
      rustc               1.63.0            -> 1.64.0
      rustc-bootstrap     1.62.1            -> 1.63.0
      setuptools          63.2.0            -> 65.3.0
      sqlite              3.39.2            -> 3.39.3
      w3m                 0.5.3+git20190105 -> 0.5.3+git20220429

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/f677051b8dc0b5e2a9348941c99eea8c4b0ff28f.tar.gz";
-  sha256 = "sha256-fSowlaoXXWcAM8m9wA6u+eTJJtvruYHMA+Lb/tFi/qM=";
+  url = "https://github.com/nixos/nixpkgs/archive/ba187fbdc5e35322c7dff556ef2c47bddfd6e8d7.tar.gz";
+  sha256 = "sha256-IBVWNJxGCsshwh62eRfR6+ry3bSXmulB3VQRzLQo3hk=";
 })
