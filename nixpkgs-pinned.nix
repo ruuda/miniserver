c13bc8dcbfaf2a051727d0d32f38b22135baaafc
Ruud van Asseldonk <dev@veniogames.com>
2022-10-30 13:24:34 +0100

    Update tzdata, nginx, and 43 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Not sure why suddenly Nginx depends on libxcrypt while it did not
    previously. It does link against it now. Aside, the binary size of the
    file system image grows by a lot, not sure why that is the case.
    
    Runtime dependencies:
    
    + libxcrypt           4.4.28
      nginx     1.23.1 -> 1.23.2
      tzdata    2022d  -> 2022e
    
    Build dependencies:
    
    - ruby2.7.6-Ascii85         1.1.0
    - ruby2.7.6-addressable     2.8.0
    - ruby2.7.6-afm             0.2.2
    - asciidoctor               2.0.17
    - ruby2.7.6-asciidoctor     2.0.17
    - ruby2.7.6-asciidoctor-pdf 1.6.2
    - bundler                   2.3.22
    - ruby2.7.6-coderay         1.1.3
    - ruby2.7.6-concurrent-ruby 1.1.9
    - ruby2.7.6-css_parser      1.11.0
      git                       2.37.3     -> 2.38.0
      git-minimal               2.37.3     -> 2.38.0
      go                        1.18.6     -> 1.19.2
    - ruby2.7.6-hashery         2.1.2
      kexec-tools               2.0.23     -> 2.0.25
      libxml2                   2.10.0     -> 2.10.2
      libxslt                   1.1.36     -> 1.1.37
    - libyaml                   0.2.5
      linux-headers             5.19       -> 6.0
    - ruby2.7.6-pdf-core        0.9.0
    - ruby2.7.6-pdf-reader      2.8.0
    - ruby2.7.6-polyglot        0.3.5
      popt                      1.18       -> 1.19
    - ruby2.7.6-prawn           2.4.0
    - ruby2.7.6-prawn-icon      3.0.0
    - ruby2.7.6-prawn-svg       0.32.0
    - ruby2.7.6-prawn-table     0.2.2
    - ruby2.7.6-prawn-templates 0.1.2
    - ruby2.7.6-public_suffix   4.0.6
    - ruby2.7.6-pygments.rb     2.3.0
    - ruby2.7.6-rexml           3.2.5
    - ruby2.7.6-rouge           3.27.0
    - ruby                      2.7.6
    - ruby2.7.6-ruby-rc4        0.1.5
    - rubygems                  3.3.20
    - ruby2.7.6-safe_yaml       1.0.5
      sqlite                    3.39.3     -> 3.39.4
      systemd-minimal           251.4      -> 251.5
    - ruby2.7.6-treetop         1.6.11
    - ruby2.7.6-ttfunk          1.7.0
      tzdata                    1.17.patch -> 1.19.patch
      xz                        5.2.6      -> 5.2.7

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/ba187fbdc5e35322c7dff556ef2c47bddfd6e8d7.tar.gz";
-  sha256 = "sha256-IBVWNJxGCsshwh62eRfR6+ry3bSXmulB3VQRzLQo3hk=";
+  url = "https://github.com/nixos/nixpkgs/archive/fdebb81f45a1ba2c4afca5fd9f526e1653ad0949.tar.gz";
+  sha256 = "sha256-xOn0ZgjImIyeecEsrjxuvlW7IW5genTwvvnDQRFncB8=";
 })
