b2d6552a1714644d477f204589b6a308849ea2a0
Ruud van Asseldonk <dev@veniogames.com>
2022-11-09 18:31:18 +0100

    Update libressl, python3-minimal, and 10 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      libressl 3.6.0 -> 3.6.1
    
    Build dependencies:
    
      curl            7.85.0  -> 7.86.0
      git             2.38.0  -> 2.38.1
      git-minimal     2.38.0  -> 2.38.1
      go              1.19.2  -> 1.19.3
      libaio          0.3.112 -> 0.3.113
      libffi          3.4.3   -> 3.4.4
      libxml2         2.10.2  -> 2.10.3
      openssl         3.0.5   -> 3.0.7
      python3         3.10.7  -> 3.10.8
      python3-minimal 3.10.7  -> 3.10.8
      zlib            1.2.12  -> 1.2.13

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/fdebb81f45a1ba2c4afca5fd9f526e1653ad0949.tar.gz";
-  sha256 = "sha256-xOn0ZgjImIyeecEsrjxuvlW7IW5genTwvvnDQRFncB8=";
+  url = "https://github.com/nixos/nixpkgs/archive/093268502280540a7f5bf1e2a6330a598ba3b7d0.tar.gz";
+  sha256 = "sha256-IkSou5ox/yZ2YUhGpk8vxd2TNU2pwRlYtir5k55NaxE=";
 })
