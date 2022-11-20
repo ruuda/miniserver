ed1637587cbeefcc40e00f6c36fd9243e57fcb60
Ruud van Asseldonk <dev@veniogames.com>
2022-11-20 19:44:45 +0100

    Update libxcrypt, tzdata, and 4 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      libxcrypt 4.4.28 -> 4.4.30
      tzdata    2022e  -> 2022f
    
    Build dependencies:
    
      expat           2.4.9  -> 2.5.0
      mercurial       6.2.3  -> 6.3.0
      openssl         1.1.1q -> 1.1.1s
      systemd-minimal 251.5  -> 251.7

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/093268502280540a7f5bf1e2a6330a598ba3b7d0.tar.gz";
-  sha256 = "sha256-IkSou5ox/yZ2YUhGpk8vxd2TNU2pwRlYtir5k55NaxE=";
+  url = "https://github.com/nixos/nixpkgs/archive/52b2ac8ae18bbad4374ff0dd5aeee0fdf1aea739.tar.gz";
+  sha256 = "sha256-rC40+/W6Hio7b/RsY8SvQPKNx4WqNcTgfYv8cUMAvJk=";
 })
