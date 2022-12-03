55d1bd8881b0824b32b854e00beae726f6cb4841
Ruud van Asseldonk <dev@veniogames.com>
2022-12-03 13:19:14 +0100

    Update lego, cmake-boot, mercurial, and 4 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      lego 4.9.0 -> 4.9.1
    
    Build dependencies:
    
      babel      2.10.3 -> 2.11.0
      cmake      3.24.2 -> 3.24.3
      cmake-boot 3.24.2 -> 3.24.3
      mercurial  6.3.0  -> 6.3.1
      openssl    1.1.1s -> 3.0.7
    - openssl    3.0.7

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/52b2ac8ae18bbad4374ff0dd5aeee0fdf1aea739.tar.gz";
-  sha256 = "sha256-rC40+/W6Hio7b/RsY8SvQPKNx4WqNcTgfYv8cUMAvJk=";
+  url = "https://github.com/nixos/nixpkgs/archive/e76c78d20685a043d23f5f9e0ccd2203997f1fb1.tar.gz";
+  sha256 = "sha256-KBfoA2fOI5+wCrm7PR+j7jHqXeTkVRPQ0m5fcKchyuU=";
 })
