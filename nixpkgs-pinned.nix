58c95785655da00e16db133cfecaf9437d51cf82
Ruud van Asseldonk <dev@veniogames.com>
2022-12-10 12:37:36 +0100

    Update iana-etc, libxcrypt, and 12 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      iana-etc  20220915 -> 20221107
      libxcrypt 4.4.30   -> 4.4.33
    
    Build dependencies:
    
      autoconf-archive 2022.02.11 -> 2022.09.03
      cargo            1.64.0     -> 1.65.0
      cargo-bootstrap  1.63.0     -> 1.64.0
      lvm2             2.03.16    -> 2.03.17
      man-db           2.10.2     -> 2.11.1
      mpfr             4.1.0      -> 4.1.1
      mpfr-stage3      4.1.0      -> 4.1.1
      pytz             2022.2.1   -> 2022.6
      rustc            1.64.0     -> 1.65.0
      rustc-bootstrap  1.63.0     -> 1.64.0
      sqlite           3.39.4     -> 3.40.0
      systemd-minimal  251.7      -> 252.1

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/e76c78d20685a043d23f5f9e0ccd2203997f1fb1.tar.gz";
-  sha256 = "sha256-KBfoA2fOI5+wCrm7PR+j7jHqXeTkVRPQ0m5fcKchyuU=";
+  url = "https://github.com/nixos/nixpkgs/archive/2787fc7d1e51404678614bf0fe92fc296746eec0.tar.gz";
+  sha256 = "sha256-riNZa0xzM1it3pzxciwALeMs+0CsBMWIW2FqulzK8vM=";
 })
