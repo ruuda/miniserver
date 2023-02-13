2fe9c7da675e2f9911aa3acaf8b6a05dba61a456
Ruud van Asseldonk <dev@veniogames.com>
2023-02-13 19:38:45 +0100

    Update lego, bash-interactive, and 54 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      lego 4.9.1 -> 4.10.0
    
    Build dependencies:
    
    + Authen-SASL                                     2.16
    + Digest-HMAC                                     1.03
    + IO-Socket-SSL                                   2.068
    + Mozilla-CA                                      20200520
    + Net-SMTP-SSL                                    1.04
    + Net-SSLeay                                      1.92
      attrs                          22.1.0        -> 22.2.0
      bash                           5.1-p16       -> 5.2-p15
      bash-interactive               5.1-p16       -> 5.2-p15
    + binutils-patchelfed-ld                          2.39
    + binutils-patchelfed-ld-wrapper                  2.39
      cargo                          1.65.0        -> 1.66.1
      cargo-bootstrap                1.64.0        -> 1.65.0
      curl                           7.86.0        -> 7.87.0
    + exceptiongroup                                  1.1.0
      flit-core                      3.7.1         -> 3.8.0
    + flit-scm                                        1.7.0
      gawk                           5.1.1         -> 5.2.1
      git                            2.38.1        -> 2.39.1
      git-minimal                    2.38.1        -> 2.39.1
      gnused                         4.8           -> 4.9
      go                             1.19.4        -> 1.19.5
      libarchive                     3.6.1         -> 3.6.2
      libkrb5                        1.20          -> 1.20.1
      libmpc-stage3                  1.2.1         -> 1.3.1
    + libnet                                          3.12
      linux-headers                  6.0           -> 6.1
      lvm2                           2.03.17       -> 2.03.18
      lxml                           4.9.1         -> 4.9.2
      mercurial                      6.3.1         -> 6.3.2
      mpfr                           4.1.1         -> 4.2.0
      mpfr-stage3                    4.1.1         -> 4.2.0
      ncurses                        6.3-p20220507 -> 6.4
      nghttp2                        1.49.0        -> 1.51.0
    - onig                           6.9.8
    + oniguruma                                       6.9.8
      pcre2                          10.40         -> 10.42
      pip                            22.2.2        -> 22.3.1
      pyelftools                     0.28          -> 0.29
      pytest                         7.1.3         -> 7.2.0
      pytest-asyncio                 0.19.0        -> 0.20.3
      pytest-mock                    3.8.2         -> 3.10.0
      pytz                           2022.6        -> 2022.7
      readline                       8.1p2         -> 8.2p1
      rhash                          1.4.2         -> 1.4.3
      rustc                          1.65.0        -> 1.66.1
      rustc-bootstrap                1.64.0        -> 1.65.0
      setuptools                     65.3.0        -> 65.6.3
      setuptools-scm                 7.0.5         -> 7.1.0
      shadow                         4.11.1        -> 4.13
      sqlite                         3.40.0        -> 3.40.1
      systemd-minimal                252.1         -> 252.4
    + tcb                                             1.2
      typing-extensions              4.3.0         -> 4.4.0
      xz                             5.2.9         -> 5.4.1

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/04f574a1c0fde90b51bf68198e2297ca4e7cccf4.tar.gz";
-  sha256 = "sha256-3MpC6yZo+Xn9cPordGz2/ii6IJpP2n8LE8e/ebUXLrs=";
+  url = "https://github.com/nixos/nixpkgs/archive/d917136f550a8c36efb1724390c7245105f79023.tar.gz";
+  sha256 = "sha256-gV/RnfVZkGLHn+5rmX2GSh5aquVHpWOJw1cnpEV03tQ=";
 })
