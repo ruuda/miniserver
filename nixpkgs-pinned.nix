ed1e3637cbec25fc600e9403f75fbb2e1506f338
Ruud van Asseldonk <dev@veniogames.com>
2023-03-16 22:33:11 +0100

    Update libunistring, go, and 33 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      libunistring 1.0 -> 1.1
    
    Build dependencies:
    
      babel             2.11.0                     -> 2.12.1
      c-ares            1.18.1                     -> 1.19.0
      cmake             3.25.1                     -> 3.25.2
      cmake-boot        3.25.1                     -> 3.25.2
      cracklib          2.9.7                      -> 2.9.8
      cryptsetup        2.5.0                      -> 2.6.1
      curl              7.88.0                     -> 7.88.1
      diffutils         3.8                        -> 3.9
      e2fsprogs         1.46.5                     -> 1.46.6
    + editables                                       0.3
      gnumake           4.4                        -> 4.4.1
      go                1.16-linux-amd64-bootstrap -> 1.17.13-linux-amd64-bootstrap
      go                1.19.6                     -> 1.20.2
    + hatchling                                       1.13.0
      iniconfig         1.1.1                      -> 2.0.0
      libcap            2.66                       -> 2.67
      libpipeline       1.5.6                      -> 1.5.7
      linux-headers     6.1                        -> 6.2
      lvm2              2.03.18                    -> 2.03.19
      man-db            2.11.1                     -> 2.11.2
      markupsafe        2.1.1                      -> 2.1.2
      mercurial         6.3.2                      -> 6.3.3
      packaging         21.3                       -> 23.0
    + pathspec                                        0.11.0
      pip               22.3.1                     -> 23.0.1
      pygments          2.13.0                     -> 2.14.0
    - pyparsing         3.0.9
      pytest            7.2.0                      -> 7.2.1
    - pytz              2022.7.1
      re2               2023-02-01                 -> 2023-03-01
      setuptools        65.6.3                     -> 67.4.0
      sqlite            3.40.1                     -> 3.41.0
      texinfo           6.8                        -> 7.0.2
      typing-extensions 4.4.0                      -> 4.5.0

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/3c5319ad3aa51551182ac82ea17ab1c6b0f0df89.tar.gz";
-  sha256 = "sha256-+AB4dYllWig8iO6vAiGGYl0NEgmMgGHpy9gzWJ3322g=";
+  url = "https://github.com/nixos/nixpkgs/archive/ac718d02867a84b42522a0ece52d841188208f2c.tar.gz";
+  sha256 = "sha256-xTICr1j+uat5hk9FyuPOFGxpWHdJRibwZC+ATi0RbtE=";
 })
