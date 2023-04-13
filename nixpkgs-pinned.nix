07ad57b1adb0cb64135a0b76a1031735577186e5
Ruud van Asseldonk <dev@veniogames.com>
2023-04-13 23:06:05 +0200

    Update iana-etc, libidn2, tzdata, and 27 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      glibc    2.35-224 -> 2.37-8
      iana-etc 20221107 -> 20230316
      libidn2  2.3.2    -> 2.3.4
      nginx    1.23.3   -> 1.23.4
      tzdata   2023b    -> 2023c
    + xgcc                 12.2.0
    
    Build dependencies:
    
    + bootstrap-stage-xgcc-gcc-wrapper
      bootstrap-stage2-gcc-wrapper              -> 12.2.0
      bootstrap-stage3-gcc-wrapper              -> 12.2.0
      cargo                            1.68.1   -> 1.68.2
    + cargo-auditable                              0.6.1
      cython                           0.29.33  -> 0.29.34
      getent-glibc                     2.35-224 -> 2.37-8
      glibc-iconv                      2.35     -> 2.37
      glibc-locales                    2.35-224 -> 2.37-8
    + gmp                                          6.2.1
    - gmp-with-cxx-stage3              6.2.1
    - gmp-with-cxx-stage4              6.2.1
      go                               1.20.2   -> 1.20.3
    + isl                                          0.20
    - isl-stage3                       0.20
    + libmpc                                       1.3.1
    - libmpc-stage3                    1.3.1
      mercurial                        6.3.3    -> 6.4
    - mpfr-stage3                      4.2.0
      pytz                             2022.7.1 -> 2023.2
      rustc                            1.68.1   -> 1.68.2
      sqlite                           3.41.1   -> 3.41.2
      squashfs                         4.6.1    -> 4.5.1
      systemd-minimal                  253.1    -> 253.2

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/ruuda/nixpkgs/archive/f9ce837059897e692bd7d31a7e1329f9af7ce254.tar.gz";
-  sha256 = "sha256-YyByJMU0FvnpOqA8wS4UBPf3IOTUNeU2JcyvYyOFpc8=";
+  url = "https://github.com/nixos/nixpkgs/archive/fe2ecaf706a5907b5e54d979fbde4924d84b65fc.tar.gz";
+  sha256 = "sha256-JEdQHsYuCfRL2PICHlOiH/2ue3DwoxUX7DJ6zZxZXFk=";
 })
