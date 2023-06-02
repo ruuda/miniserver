fda659e232639bcc1b7db66187077bfd2f341ad5
Ruud van Asseldonk <dev@veniogames.com>
2023-06-03 00:47:20 +0200

    Update lego, util-linux-minimal, and 7 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      lego 4.11.0 -> 4.12.0
    
    Build dependencies:
    
      audit              3.1    -> 3.1.1
      curl               8.0.1  -> 8.1.1
      e2fsprogs          1.46.6 -> 1.47.0
      gnugrep            3.7    -> 3.11
      libcap             2.68   -> 2.69
      libuv              1.44.2 -> 1.45.0
      linux-headers      6.2    -> 6.3
      util-linux-minimal 2.38.1 -> 2.39

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/f91ee3065de91a3531329a674a45ddcb3467a650.tar.gz";
-  sha256 = "sha256-6QMMsXMr2nhmOPHdti2j3KRHt+bai2zw+LJfdCl97Mk=";
+  url = "https://github.com/nixos/nixpkgs/archive/e635192892f5abbc2289eaac3a73cdb249abaefd.tar.gz";
+  sha256 = "sha256-6EujQNAeaUkWvpEZZcVF8qSfQrNVWFNNGbUJxv/A5a8=";
 })
