11feb442dc6ac504d01ae546244ec29a9b6d308e
Ruud van Asseldonk <dev@veniogames.com>
2023-08-24 00:04:43 +0200

    Update nginx to 1.25.2, and 36 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      nginx 1.25.1 -> 1.25.2
    
    Build dependencies:
    
      auditable-cargo-bootstrap 1.69.0     -> 1.70.0
    + calver                                  2022.06.26
      cargo                     1.70.0     -> 1.71.1
      cargo-bootstrap           1.69.0     -> 1.70.0
      curl                      8.1.2      -> 8.2.1
      flit-core                 3.8.0      -> 3.9.0
      gmp                       6.2.1      -> 6.3.0
      gmp-with-cxx              6.2.1      -> 6.3.0
      gnu-config                2023-01-21 -> 2023-07-31
      gnutar                    1.34       -> 1.35
      go                        1.20.6     -> 1.20.7
      groff                     1.22.4     -> 1.23.0
      hatchling                 1.13.0     -> 1.18.0
      kbd                       2.5.1      -> 2.6.1
      libpfm                    4.12.0     -> 4.13.0
      lxml                      4.9.2      -> 4.9.3-3
      mercurial                 6.5        -> 6.5.1
      meson                     1.1.1      -> 1.2.0
      nghttp2                   1.51.0     -> 1.54.0
      nss-cacert                3.90       -> 3.92
      nss-cacert-certdata       3.90       -> 3.92
      openssl                   3.0.9      -> 3.0.10
      packaging                 23.0       -> 23.1
      perl                      5.36.0     -> 5.38.0
      pluggy                    1.0.0      -> 1.2.0
      pygments                  2.14.0     -> 2.15.1
      pytest                    7.2.1      -> 7.4.0
      pytest-asyncio            0.20.3     -> 0.21.1
      pytest-mock               3.10.0     -> 3.11.1
      re2c                      3.0        -> 3.1
      rhash                     1.4.3      -> 1.4.4
      rustc                     1.70.0     -> 1.71.1
      rustc-bootstrap           1.69.0     -> 1.70.0
    + trove-classifiers                       2023.7.6
      typing-extensions         4.5.0      -> 4.7.1
      xz                        5.4.3      -> 5.4.4

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/9e1960bc196baf6881340d53dccb203a951745a2.tar.gz";
-  sha256 = "sha256-h/nXluEqdiQHs1oSgkOOWF+j8gcJMWhwnZ9PFabN6q0=";
+  url = "https://github.com/nixos/nixpkgs/archive/91a22f76cd1716f9d0149e8a5c68424bb691de15.tar.gz";
+  sha256 = "sha256-J0LgSFgJVGCC1+j5R2QndadWI1oumusg6hCtYAzLID4=";
 })
