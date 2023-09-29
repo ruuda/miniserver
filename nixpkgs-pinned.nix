b04ed4c874303f58c05eeae09c9c22a68ba3db23
Ruud van Asseldonk <dev@veniogames.com>
2023-09-29 19:59:11 +0200

    Update zlib-ng, lego, and 29 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      lego    4.14.0 -> 4.14.2
      zlib-ng 2.1.2  -> 2.1.3
    
    Build dependencies:
    
      audit                     3.1.1      -> 3.1.2
      auditable-cargo-bootstrap 1.70.0     -> 1.71.1
      autoconf-archive          2022.09.03 -> 2023.02.20
      brotli                    1.0.9      -> 1.1.0
      cargo                     1.71.1     -> 1.72.0
      cargo-bootstrap           1.70.0     -> 1.71.1
      cracklib                  2.9.8      -> 2.9.11
      curl                      8.2.1      -> 8.3.0
      git                       2.41.0     -> 2.42.0
      git-minimal               2.41.0     -> 2.42.0
      go                        1.20.7     -> 1.20.8
      gzip                      1.12       -> 1.13
      jq                        1.6        -> 1.7
      kbd                       2.6.1      -> 2.6.2
    + libbsd                                  unstable-2023-04-29
    + libmd                                   1.1.0
      libxml2                   2.11.4     -> 2.11.5
      linux-headers             6.4        -> 6.5
      lvm2                      2.03.21    -> 2.03.22
      mercurial                 6.5.1      -> 6.5.2
      mpfr                      4.2.0      -> 4.2.1
      re2                       2023-08-01 -> 2023-09-01
      rustc                     1.71.1     -> 1.72.0
      rustc-bootstrap           1.70.0     -> 1.71.1
      shadow                    4.13       -> 4.14.0
      sqlite                    3.42.0     -> 3.43.1
      systemd-minimal           253.6      -> 254.3
      util-linux-minimal        2.39.1     -> 2.39.2
      zlib                      1.2.13     -> 1.3

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/a999c1cc0c9eb2095729d5aa03e0d8f7ed256780.tar.gz";
-  sha256 = "sha256-aRTTXkYvhXosGx535iAFUaoFboUrZSYb1Ooih/auGp0=";
+  url = "https://github.com/nixos/nixpkgs/archive/8a86b98f0ba1c405358f1b71ff8b5e1d317f5db2.tar.gz";
+  sha256 = "sha256-gToZXQVr0G/1WriO83olnqrLSHF2Jb8BPcmCt497ro0=";
 })
