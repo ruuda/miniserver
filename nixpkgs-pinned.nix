94907283f7664103202988d5c73ebe0ef350c363
Ruud van Asseldonk <dev@veniogames.com>
2023-12-03 09:55:23 +0100

    Update zlib-ng to 2.1.5
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      zlib-ng 2.1.4 -> 2.1.5

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/e4ad989506ec7d71f7302cc3067abd82730a4beb.tar.gz";
-  sha256 = "sha256-de9KYi8rSJpqvBfNwscWdalIJXPo8NjdIZcEJum1mH0=";
+  url = "https://github.com/nixos/nixpkgs/archive/e92039b55bcd58469325ded85d4f58dd5a4eaf58.tar.gz";
+  sha256 = "sha256-ztaDIyZ7HrTAfEEUt9AtTDNoCYxUdSd6NrRHaYOIxtk=";
 })
