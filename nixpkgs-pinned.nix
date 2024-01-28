6761512e6ce410df5583573423255d694e8faedf
Ruud van Asseldonk <dev@veniogames.com>
2024-01-28 22:05:45 +0100

    Update iana-etc to 20231227, and 12 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      iana-etc 20230316 -> 20231227
    
    Build dependencies:
    
    - Jinja2               3.1.2
      autoconf             2.71    -> 2.72
      curl                 8.4.0   -> 8.5.0
      go                   1.21.5  -> 1.21.6
    + jinja2                          3.1.3
      libcap-ng            0.8.3   -> 0.8.4
      linux-pam            1.5.2   -> 1.5.3
      lvm2                 2.03.22 -> 2.03.23
      mercurial            6.6.1   -> 6.6.2
      meson                1.3.0   -> 1.3.1
      readline             8.2p7   -> 8.2p10
      systemd-minimal-libs 254.6   -> 255.2

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/612f97239e2cc474c13c9dafa0df378058c5ad8d.tar.gz";
-  sha256 = "sha256-JXfnuEf5Yd6bhMs/uvM67/joxYKoysyE3M2k6T3eWbg=";
+  url = "https://github.com/nixos/nixpkgs/archive/ae5c332cbb5827f6b1f02572496b141021de335f.tar.gz";
+  sha256 = "sha256-eLihrZAPZX0R6RyM5fYAWeKVNuQPYjAkCUBr+JNvtdE=";
 })
