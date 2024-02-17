8a045103dcd3f4cf8d770070fde4debde14927a8
Ruud van Asseldonk <dev@veniogames.com>
2024-02-17 12:12:42 +0100

    Update libidn2, glibc, tzdata, and 33 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      glibc   2.38-27 -> 2.38-44
      libidn2 2.3.4   -> 2.3.7
      tzdata  2023d   -> 2024a
    
    Build dependencies:
    
      abseil-cpp        20230125.3                 -> 20240116.0
      acl               2.3.1                      -> 2.3.2
      attr              2.5.1                      -> 2.5.2
      bash              5.2-p21                    -> 5.2p26
      bash-interactive  5.2-p21                    -> 5.2p26
      boehm-gc          8.2.2                      -> 8.2.4
      cmake             3.27.8                     -> 3.27.9
      cmake-minimal     3.27.8                     -> 3.27.9
      cpio              2.14                       -> 2.15
      cryptsetup        2.6.1                      -> 2.7.0
      curl              8.5.0                      -> 8.6.0
      ed                1.19                       -> 1.20
      getent-glibc      2.38-27                    -> 2.38-44
      glibc-locales     2.38-27                    -> 2.38-44
      hatchling         1.21.0                     -> 1.21.1
      kbd               2.6.3                      -> 2.6.4
    + libpsl                                          0.21.5
      libxml2           2.12.3-unstable-2023-12-14 -> 2.12.4
      linux-headers     6.6                        -> 6.7
      linux-pam         1.5.3                      -> 1.6.0
    + mpdecimal                                       2.5.1
      nghttp2           1.57.0                     -> 1.59.0
      openssl           3.0.12                     -> 3.0.13
      pluggy            1.3.0                      -> 1.4.0
      psutil            5.9.6                      -> 5.9.8
    + publicsuffix-list                               0-unstable-2024-01-07
      pytest            7.4.3                      -> 7.4.4
      setuptools        69.0.2                     -> 69.0.3
      shadow            4.14.2                     -> 4.14.3
      sqlite            3.44.2                     -> 3.45.1
      trove-classifiers 2023.11.29                 -> 2024.1.8
      xz                5.4.5                      -> 5.4.6
      zlib              1.3                        -> 1.3.1

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/ae5c332cbb5827f6b1f02572496b141021de335f.tar.gz";
-  sha256 = "sha256-eLihrZAPZX0R6RyM5fYAWeKVNuQPYjAkCUBr+JNvtdE=";
+  url = "https://github.com/nixos/nixpkgs/archive/a4d4fe8c5002202493e87ec8dbc91335ff55552c.tar.gz";
+  sha256 = "sha256-ZL2TrjVsiFNKOYwYQozpbvQSwvtV/3Me7Zwhmdsfyu4=";
 })
