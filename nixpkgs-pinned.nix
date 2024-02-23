b7de28d3051fbfdbde46a2ac75fb1f1bea7c985b
Ruud van Asseldonk <dev@veniogames.com>
2024-02-23 23:58:13 +0100

    Update nginx to 1.25.4
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      nginx 1.25.3 -> 1.25.4

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/a4d4fe8c5002202493e87ec8dbc91335ff55552c.tar.gz";
-  sha256 = "sha256-ZL2TrjVsiFNKOYwYQozpbvQSwvtV/3Me7Zwhmdsfyu4=";
+  url = "https://github.com/nixos/nixpkgs/archive/cbc4211f0afffe6dfd2478a62615dd5175a13f9a.tar.gz";
+  sha256 = "sha256-ZrP/yACUvDB+zbqYJsln4iwotbH6CTZiTkANJ0AgDv4=";
 })
