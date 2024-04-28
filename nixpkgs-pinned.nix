588e7aaf8d881004d892b3eaa444d48390f3f4ea
Ruud van Asseldonk <dev@veniogames.com>
2024-04-28 18:27:34 +0200

    Update iana-etc to 20240318, and 25 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      iana-etc 20231227 -> 20240318
    
    Build dependencies:
    
      abseil-cpp                20240116.1   -> 20240116.2
      auditable-cargo-bootstrap 1.75.0       -> 1.76.0
      cargo                     1.76.0       -> 1.77.1
      cargo-bootstrap           1.75.0       -> 1.76.0
      cmake                     3.28.3       -> 3.29.1
      cmake-minimal             3.28.3       -> 3.29.1
      coreutils                 9.4          -> 9.5
      cryptsetup                2.7.0        -> 2.7.1
      curl                      8.6.0        -> 8.7.1
      go                        1.22.1       -> 1.22.2
      hatchling                 1.21.1       -> 1.22.4
      kexec-tools               2.0.26       -> 2.0.28
      man-db                    2.12.0       -> 2.12.1
      meson                     1.3.2        -> 1.4.0
      nghttp2                   1.60.0       -> 1.61.0
      pytest                    8.0.2        -> 8.1.1
      pytest-asyncio            0.23.5.post1 -> 0.23.6
      python3                   3.11.8       -> 3.11.9
      python3-minimal           3.11.8       -> 3.11.9
      rustc                     1.76.0       -> 1.77.1
      rustc-bootstrap           1.75.0       -> 1.76.0
      rustc-bootstrap-wrapper   1.75.0       -> 1.76.0
      rustc-wrapper             1.76.0       -> 1.77.1
      setuptools                69.1.1       -> 69.2.0
      zstd                      1.5.5        -> 1.5.6

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/66adc1e47f8784803f2deb6cacd5e07264ec2d5c.tar.gz";
-  sha256 = "sha256-hOkzkhLT59wR8VaMbh1ESjtZLbGi+XNaBN6h49SPqEc=";
+  url = "https://github.com/nixos/nixpkgs/archive/58a1abdbae3217ca6b702f03d3b35125d88a2994.tar.gz";
+  sha256 = "sha256-mdTQw2XlariysyScCv2tTE45QSU9v/ezLcHJ22f0Nxc=";
 })
