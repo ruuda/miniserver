eaaa073dbb052d92b3c3f715af0f4ba9c5d9cfc3
Ruud van Asseldonk <dev@veniogames.com>
2024-05-27 20:25:29 +0200

    Update libressl, glibc, lego, and 20 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      glibc    2.39-31 -> 2.39-52
      lego     4.15.0  -> 4.16.1
      libressl 3.9.1   -> 3.9.2
    
    Build dependencies:
    
      cargo                1.77.1     -> 1.77.2
      docutils             0.21.1     -> 0.21.2
      ed                   1.20.1     -> 1.20.2
      getent-glibc         2.39-31    -> 2.39-52
      git                  2.44.0     -> 2.44.1
      git-minimal          2.44.0     -> 2.44.1
      glibc-locales        2.39-31    -> 2.39-52
      gnu-config           2023-09-19 -> 2024-01-01
      go                   1.22.2     -> 1.22.3
      hatchling            1.24.1     -> 1.24.2
      jinja2               3.1.3      -> 3.1.4
      libarchive           3.7.3      -> 3.7.4
      libxml2              2.12.6     -> 2.12.7
      re2                  2024-04-01 -> 2024-05-01
      rustc                1.77.1     -> 1.77.2
      rustc-wrapper        1.77.1     -> 1.77.2
      sqlite               3.45.2     -> 3.45.3
      systemd-minimal-libs 255.4      -> 255.6
      util-linux-minimal   2.39.3     -> 2.40.1
      wheel                0.42.0     -> 0.43.0

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/25865a40d14b3f9cf19f19b924e2ab4069b09588.tar.gz";
-  sha256 = "sha256-UlRZtrCnhPFSJlDQE7M0eyhgvuuHBTe1eJ9N9AQlJQ0=";
+  url = "https://github.com/nixos/nixpkgs/archive/bfb7a882678e518398ce9a31a881538679f6f092.tar.gz";
+  sha256 = "sha256-4zSIhSRRIoEBwjbPm3YiGtbd8HDWzFxJjw5DYSDy1n8=";
 })
