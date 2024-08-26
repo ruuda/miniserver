8ab1d073b0d2bbb9a1fc29914f214e9c92dd074f
Ruud van Asseldonk <dev@veniogames.com>
2024-08-26 22:10:35 +0200

    Update nginx, publicsuffix-list, and 20 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      nginx 1.27.0 -> 1.27.1
    
    Build dependencies:
    
      abseil-cpp                20240116.2            -> 20240722.0
      auditable-cargo-bootstrap 1.78.0                -> 1.79.0
      bash                      5.2p26                -> 5.2p32
      bash-interactive          5.2p26                -> 5.2p32
      cargo                     1.79.0                -> 1.80.1
      cargo-bootstrap           1.78.0                -> 1.79.0
      curl                      8.8.0                 -> 8.9.0
      gdbm                      1.23                  -> 1.24
      libxml2                   2.13.2                -> 2.13.3
      mercurial                 6.8                   -> 6.8.1
      meson                     1.4.1                 -> 1.5.1
    + patchutils                                         0.4.2
      publicsuffix-list         0-unstable-2024-01-07 -> 0-unstable-2024-06-19
      pytest                    8.2.2                 -> 8.3.2
      rustc                     1.79.0                -> 1.80.1
      rustc-bootstrap           1.78.0                -> 1.79.0
      rustc-bootstrap-wrapper   1.78.0                -> 1.79.0
      rustc-wrapper             1.79.0                -> 1.80.1
      setuptools                70.0.0                -> 72.1.0
      systemd-minimal-libs      256.2                 -> 256.4
      xmlto                     0.0.28                -> 0.0.29

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/52ec9ac3b12395ad677e8b62106f0b98c1f8569d.tar.gz";
-  sha256 = "sha256-veKR07psFoJjINLC8RK4DiLniGGMgF3QMlS4tb74S6k=";
+  url = "https://github.com/nixos/nixpkgs/archive/d0e1602ddde669d5beb01aec49d71a51937ed7be.tar.gz";
+  sha256 = "sha256-pP3Azj5d6M5nmG68Fu4JqZmdGt4S4vqI5f8te+E/FTw=";
 })
