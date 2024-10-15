bb42ad2cce442f326af3b3ba461f524098b3ebac
Ruud van Asseldonk <dev@veniogames.com>
2024-10-15 23:06:41 +0200

    Update zlib-ng, glibc, tzdata, and 53 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      glibc   2.39-52 -> 2.40-36
      lego    4.17.4  -> 4.19.2
      nginx   1.27.1  -> 1.27.2
      tzdata  2024a   -> 2024b
      zlib-ng 2.2.1   -> 2.2.2
    
    Build dependencies:
    
      auditable-cargo-bootstrap      1.79.0                -> 1.80.1
      binutils                       2.42                  -> 2.43.1
      binutils-patchelfed-ld         2.42                  -> 2.43.1
      binutils-patchelfed-ld-wrapper 2.42                  -> 2.43.1
      binutils-wrapper               2.42                  -> 2.43.1
      build                          1.2.1                 -> 1.2.2
    + byacc                                                   20240109
      cargo                          1.80.1                -> 1.81.0
      cargo-bootstrap                1.79.0                -> 1.80.1
      cryptsetup                     2.7.3                 -> 2.7.4
      curl                           8.9.0                 -> 8.9.1
      cython                         3.0.10                -> 3.0.11
      expat                          2.6.2                 -> 2.6.3
      gbenchmark                     1.8.4                 -> 1.9.0
      getent-glibc                   2.39-52               -> 2.40-36
      git                            2.45.2                -> 2.46.1
      git-minimal                    2.45.2                -> 2.46.1
      glibc-iconv                    2.39                  -> 2.40
      glibc-locales                  2.39-52               -> 2.40-36
      go                             1.22.5                -> 1.23.2
      gtest                          1.14.0                -> 1.15.2
      kexec-tools                    2.0.28                -> 2.0.29
    + krb5                                                    1.21.3
    + libedit                                                 20240808-3.1
    - libkrb5                        1.21.3
      libpipeline                    1.5.7                 -> 1.5.8
      libxml2                        2.13.3                -> 2.13.4
      linux-headers                  6.9                   -> 6.10
      lvm2                           2.03.23               -> 2.03.26
      lxml                           5.2.2                 -> 5.3.0
      lz4                            1.9.4                 -> 1.10.0
      meson                          1.5.1                 -> 1.5.2
      nghttp2                        1.62.1                -> 1.63.0
      nss-cacert                     3.101.1               -> 3.104
      nss-cacert-certdata            3.101.1               -> 3.104
      openssl                        3.0.14                -> 3.3.2
    + perl                                                    5.40.0
      publicsuffix-list              0-unstable-2024-06-19 -> 0-unstable-2024-09-10
      python3                        3.12.4                -> 3.12.6
      python3-minimal                3.12.4                -> 3.12.6
      python3.12-bootstrap-build     1.2.1                 -> 1.2.2
      pytz                           2024.1                -> 2024.2
      readline                       8.2p10                -> 8.2p13
      rustc                          1.80.1                -> 1.81.0
      rustc-bootstrap                1.79.0                -> 1.80.1
      rustc-bootstrap-wrapper        1.79.0                -> 1.80.1
      rustc-wrapper                  1.80.1                -> 1.81.0
      sqlite                         3.46.0                -> 3.46.1
      swig                           3.0.12                -> 4.2.1
      systemd-minimal-libs           256.4                 -> 256.6
      trove-classifiers              2024.7.2              -> 2024.9.12

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/d0e1602ddde669d5beb01aec49d71a51937ed7be.tar.gz";
-  sha256 = "sha256-pP3Azj5d6M5nmG68Fu4JqZmdGt4S4vqI5f8te+E/FTw=";
+  url = "https://github.com/nixos/nixpkgs/archive/a3c0b3b21515f74fd2665903d4ce6bc4dc81c77c.tar.gz";
+  sha256 = "sha256-nsNdSldaAyu6PE3YUA+YQLqUDJh+gRbBooMMekZJwvI=";
 })
