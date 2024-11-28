b714701887536b4106f1c90216a5e89ed278d285
Ruud van Asseldonk <dev@veniogames.com>
2024-11-28 17:34:30 +0100

    Update to latest commit in nixos/nixpkgs nixos-unstable
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
    - pcre  8.45
    + pcre2         10.44
    
    Build dependencies:
    
      auditable-cargo-bootstrap            1.80.1                -> 1.82.0
    + auto-patchelf                                                 0-unstable-2024-08-14
      autoconf-archive                     2023.02.20            -> 2024.10.16
      babel                                2.15.0                -> 2.16.0
      bash                                 5.2p32                -> 5.2p37
      bash-interactive                     5.2p32                -> 5.2p37
      boehm-gc                             8.2.6                 -> 8.2.8
      build                                1.2.2                 -> 1.2.2.post1
      cargo                                1.81.0                -> 1.82.0
      cargo-bootstrap                      1.80.1                -> 1.82.0
      cmake                                3.29.6                -> 3.30.5
      cmake-minimal                        3.29.6                -> 3.30.5
      cryptsetup                           2.7.4                 -> 2.7.5
      curl                                 8.9.1                 -> 8.11.0
      cython                               3.0.11                -> 3.0.11-1
      expat                                2.6.3                 -> 2.6.4
      gawk                                 5.2.2                 -> 5.3.1
      git                                  2.46.1                -> 2.47.0
      git-minimal                          2.46.1                -> 2.47.0
      go                                   1.23.2                -> 1.23.3
      libarchive                           3.7.4                 -> 3.7.7
      libssh2                              1.11.0                -> 1.11.1
      lvm2                                 2.03.26               -> 2.03.27
      man-db                               2.12.1                -> 2.13.0
      markupsafe                           2.1.5                 -> 3.0.2
      mercurial                            6.8.1                 -> 6.8.2
      meson                                1.5.2                 -> 1.6.0
      nghttp2                              1.63.0                -> 1.64.0
    - pcre2                                10.44
      perl                                 5.38.2                -> 5.40.0
    - perl                                 5.40.0
      publicsuffix-list                    0-unstable-2024-09-10 -> 0-unstable-2024-10-25
      pyproject-hooks                      1.0.0                 -> 1.2.0
      pytest                               8.3.2                 -> 8.3.3
      python3                              3.12.6                -> 3.12.7
      python3-minimal                      3.12.6                -> 3.12.7
      python3.12-bootstrap-build           1.2.2                 -> 1.2.2.post1
      python3.12-bootstrap-pyproject-hooks 1.0.0                 -> 1.2.0
      rustc                                1.81.0                -> 1.82.0
      rustc-bootstrap                      1.80.1                -> 1.82.0
      rustc-bootstrap-wrapper              1.80.1                -> 1.82.0
      rustc-wrapper                        1.81.0                -> 1.82.0
      setuptools                           72.1.0                -> 75.1.0
      systemd-minimal-libs                 256.6                 -> 256.7
      texinfo                              7.1                   -> 7.1.1
      wheel                                0.43.0                -> 0.44.0
      xz                                   5.6.2                 -> 5.6.3

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/1997e4aa514312c1af7e2bda7fad1644e778ff26.tar.gz";
-  sha256 = "sha256-I4tuhRpZFa6Fu6dcH9Dlo5LlH17peT79vx1y1SpeKt0=";
+  url = "https://github.com/nixos/nixpkgs/archive/4633a7c72337ea8fd23a4f2ba3972865e3ec685d.tar.gz";
+  sha256 = "sha256-2ThgXBUXAE1oFsVATK1ZX9IjPcS4nKFOAjhPNKuiMn0=";
 })
