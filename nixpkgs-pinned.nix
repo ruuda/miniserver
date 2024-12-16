2534f65c74c2248653f512cdc5689dbcb7b54954
Ruud van Asseldonk <dev@veniogames.com>
2024-12-16 18:45:25 +0100

    Update nginx, systemd-minimal-libs, and 4 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      nginx 1.27.2 -> 1.27.3
    
    Build dependencies:
    
      nss-cacert           3.104  -> 3.107
      nss-cacert-certdata  3.104  -> 3.107
      setuptools           75.1.0 -> 75.1.1
      systemd-minimal-libs 256.7  -> 256.8
      tcl                  8.6.13 -> 8.6.15

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/4633a7c72337ea8fd23a4f2ba3972865e3ec685d.tar.gz";
-  sha256 = "sha256-2ThgXBUXAE1oFsVATK1ZX9IjPcS4nKFOAjhPNKuiMn0=";
+  url = "https://github.com/nixos/nixpkgs/archive/3566ab7246670a43abd2ffa913cc62dad9cdf7d5.tar.gz";
+  sha256 = "sha256-AKU6qqskl0yf2+JdRdD0cfxX4b9x3KKV5RqA6wijmPM=";
 })
