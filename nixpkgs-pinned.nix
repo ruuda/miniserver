7883bfe34496d2e7de75841ef90407cafff8d50f
Ruud van Asseldonk <dev@veniogames.com>
2024-12-29 16:32:32 +0100

    Update xgcc, libunistring, and 38 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      libunistring 1.2    -> 1.3
      xgcc         13.3.0 -> 14-20241116
    
    Build dependencies:
    
      audit                          4.0                   -> 4.0.2
      auditable-cargo-bootstrap      1.82.0                -> 1.83.0
      bootstrap-stage2-gcc-wrapper   13.3.0                -> 14-20241116
      bootstrap-stage3-gcc-wrapper   13.3.0                -> 14-20241116
      bootstrap-stage4-gcc-wrapper   13.3.0                -> 14-20241116
      cargo                          1.82.0                -> 1.83.0
      cargo-auditable                0.6.2                 -> 0.6.5
      cargo-bootstrap                1.82.0                -> 1.83.0
      file                           5.45                  -> 5.46
      flit-core                      3.9.0                 -> 3.10.0
      gcc                            13.3.0                -> 14-20241116
      gcc-wrapper                    13.3.0                -> 14-20241116
      gettext                        0.21.1                -> 0.22.5
      go                             1.23.3                -> 1.23.4
      hatchling                      1.25.0                -> 1.26.1
      json-c                         0.17                  -> 0.18
      libuv                          1.48.0                -> 1.49.2
      libxml2                        2.13.4                -> 2.13.5
      linux-headers                  6.10                  -> 6.12
      llvm                           18.1.8                -> 19.1.5
      llvm-src                       18.1.8                -> 19.1.5
      lvm2                           2.03.27               -> 2.03.28
      packaging                      24.1                  -> 24.2
      publicsuffix-list              0-unstable-2024-10-25 -> 0-unstable-2024-12-06
      python3                        3.12.7                -> 3.12.8
      python3-minimal                3.12.7                -> 3.12.8
      python3.12-bootstrap-flit-core 3.9.0                 -> 3.10.0
      python3.12-bootstrap-packaging 24.1                  -> 24.2
      re2c                           3.1                   -> 4.0.1
      rustc                          1.82.0                -> 1.83.0
      rustc-bootstrap                1.82.0                -> 1.83.0
      rustc-bootstrap-wrapper        1.82.0                -> 1.83.0
      rustc-wrapper                  1.82.0                -> 1.83.0
      setuptools                     75.1.1                -> 75.3.0
      six                            1.16.0                -> 1.17.0
      sqlite                         3.46.1                -> 3.47.0
      swig                           4.2.1                 -> 4.3.0
      wheel                          0.44.0                -> 0.45.1

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/3566ab7246670a43abd2ffa913cc62dad9cdf7d5.tar.gz";
-  sha256 = "sha256-AKU6qqskl0yf2+JdRdD0cfxX4b9x3KKV5RqA6wijmPM=";
+  url = "https://github.com/nixos/nixpkgs/archive/634fd46801442d760e09493a794c4f15db2d0cbb.tar.gz";
+  sha256 = "sha256-NYVcA06+blsLG6wpAbSPTCyLvxD/92Hy4vlY9WxFI1M=";
 })
