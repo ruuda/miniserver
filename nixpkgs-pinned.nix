4ba46a200599eb4dc100bb81fe33f1c7500feaa0
Ruud van Asseldonk <dev@veniogames.com>
2025-01-31 21:17:59 +0100

    Update zlib-ng, publicsuffix-list, and 27 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      zlib-ng 2.2.2 -> 2.2.3
    
    Build dependencies:
    
      byacc                20240109              -> 20241231
      cmake                3.30.5                -> 3.31.3
      cmake-minimal        3.30.5                -> 3.31.3
      curl                 8.11.0                -> 8.11.1
      gbenchmark           1.9.0                 -> 1.9.1
      git                  2.47.0                -> 2.47.1
      git-minimal          2.47.0                -> 2.47.1
      hatchling            1.26.1                -> 1.27.0
      jinja2               3.1.4                 -> 3.1.5
      kbd                  2.6.4                 -> 2.7.1
      libcap               2.70                  -> 2.73
      libtool              2.4.7                 -> 2.5.4
      linux-headers        6.12                  -> 6.12.7
      llvm                 19.1.5                -> 19.1.6
      llvm-src             19.1.5                -> 19.1.6
    + llvm-tblgen                                   19.1.6
    + llvm-tblgen-src                               19.1.6
      lvm2                 2.03.28               -> 2.03.29
      mercurial            6.8.2                 -> 6.9
      meson                1.6.0                 -> 1.6.1
      oniguruma            6.9.9                 -> 6.9.10
    - patchutils           0.4.2
      psutil               6.0.0                 -> 6.1.1
      publicsuffix-list    0-unstable-2024-12-06 -> 0-unstable-2024-12-24
      re2c                 4.0.1                 -> 4.0.2
      shadow               4.16.0                -> 4.17.2
      sqlite               3.47.0                -> 3.47.2
      systemd-minimal-libs 256.8                 -> 257.2

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/634fd46801442d760e09493a794c4f15db2d0cbb.tar.gz";
-  sha256 = "sha256-NYVcA06+blsLG6wpAbSPTCyLvxD/92Hy4vlY9WxFI1M=";
+  url = "https://github.com/nixos/nixpkgs/archive/9d3ae807ebd2981d593cddd0080856873139aa40.tar.gz";
+  sha256 = "sha256-NGqpVVxNAHwIicXpgaVqJEJWeyqzoQJ9oc8lnK9+WC4=";
 })
