0c22a56ae1f6a995ec5d3288db7cbe9200f4f854
Ruud van Asseldonk <dev@veniogames.com>
2025-02-21 18:49:59 +0100

    Update nginx to 1.27.4, and 65 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      nginx 1.27.3 -> 1.27.4
    
    Build dependencies:
    
    - asciidoctor             2.0.23
    - auditable-cargo         1.84.0
    - aws-c-auth              0.8.0
    - aws-c-cal               0.8.0
    - aws-c-common            0.10.3
    - aws-c-compression       0.3.0
    - aws-c-event-stream      0.5.0
    - aws-c-http              0.9.2
    - aws-c-io                0.15.3
    - aws-c-mqtt              0.11.0
    - aws-c-s3                0.7.1
    - aws-c-sdkutils          0.2.1
    - aws-checksums           0.2.2
    - aws-crt-cpp             0.29.4
    - aws-sdk-cpp             1.11.448
    - boost                   1.87.0
    - boost-build             boost-1.87.0
    - bundler                 2.5.22
    - busybox                 1.36.1
    - editline                1.17.1
    - git-lfs                 3.6.1
    - libcpuid                0.7.1
    - libgit2                 1.9.0
    - libsodium               1.0.20
    - libyaml                 0.2.5
    - llhttp                  9.2.1
    - lowdown                 1.3.2
    - mdbook                  0.4.43
    - mdbook                  0.4.43-vendor
    - mdbook-linkcheck        0.7.7
    - mdbook-linkcheck        0.7.7-vendor
    - musl                    1.2.5
    - nix                     2.24.12
    - nlohmann_json           3.11.3
    - rapidcheck              0-unstable-2023-12-14
    - ruby                    3.3.6
    - ruby3.3-Ascii85         1.1.1
    - ruby3.3-addressable     2.8.7
    - ruby3.3-afm             0.2.2
    - ruby3.3-asciidoctor     2.0.23
    - ruby3.3-asciidoctor-pdf 2.3.19
    - ruby3.3-coderay         1.1.3
    - ruby3.3-concurrent-ruby 1.3.4
    - ruby3.3-css_parser      1.19.1
    - ruby3.3-hashery         2.1.2
    - ruby3.3-matrix          0.4.2
    - ruby3.3-pdf-core        0.9.0
    - ruby3.3-pdf-reader      2.12.0
    - ruby3.3-polyglot        0.3.5
    - ruby3.3-prawn           2.4.0
    - ruby3.3-prawn-icon      3.0.0
    - ruby3.3-prawn-svg       0.34.2
    - ruby3.3-prawn-table     0.2.2
    - ruby3.3-prawn-templates 0.1.2
    - ruby3.3-public_suffix   6.0.1
    - ruby3.3-pygments.rb     3.0.0
    - ruby3.3-rexml           3.3.9
    - ruby3.3-rouge           4.4.0
    - ruby3.3-ruby-rc4        0.1.5
    - ruby3.3-tilt            2.4.0
    - ruby3.3-treetop         1.6.12
    - ruby3.3-ttfunk          1.7.0
    - rubygems                3.5.22
    - s2n-tls                 1.5.10
    - toml11                  3.7.1

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/550e11f27ba790351d390d9eca3b80ad0f0254e7.tar.gz";
-  sha256 = "sha256-U3SNq+waitGIotmgg/Et3J7o4NvUtP2gb2VhME5QXiw=";
+  url = "https://github.com/nixos/nixpkgs/archive/73cf49b8ad837ade2de76f87eb53fc85ed5d4680.tar.gz";
+  sha256 = "sha256-EO1ygNKZlsAC9avfcwHkKGMsmipUk1Uc0TbrEZpkn64=";
 })
