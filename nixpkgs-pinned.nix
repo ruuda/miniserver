2c7dca274f1af076b30bf57da95c1ab3de83ab08
Ruud van Asseldonk <dev@veniogames.com>
2025-05-09 22:31:12 +0200

    Update lego, systemd-minimal-libs, and 21 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      lego 4.22.2 -> 4.23.1
    
    Build dependencies:
    
      abseil-cpp           20240722.1   -> 20250127.1
      c-ares               1.34.4       -> 1.34.5
      coreutils            9.6          -> 9.7
    - cracklib             2.10.0
    - db                   5.3.28
      diffutils            3.10         -> 3.12
      ed                   1.21         -> 1.21.1
      gawk                 5.3.1        -> 5.3.2
      gdbm                 1.24         -> 1.25
      gzip                 1.13         -> 1.14
      libffi               3.4.7        -> 3.4.8
      libxml2              2.13.6       -> 2.13.8
      mercurial            6.9.1        -> 6.9.4
      mercurial            6.9.1-vendor -> 6.9.4-vendor
      meson                1.7.0        -> 1.7.2
      mpfr                 4.2.1        -> 4.2.2
      python3              3.12.9       -> 3.12.10
      python3-minimal      3.12.9       -> 3.12.10
      re2c                 4.0.2        -> 4.1
      systemd-minimal-libs 257.3        -> 257.5
      texinfo              7.1.1        -> 7.2
      util-linux-minimal   2.40.4       -> 2.41

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/f771eb401a46846c1aebd20552521b233dd7e18b.tar.gz";
-  sha256 = "sha256-ITSpPDwvLBZBnPRS2bUcHY3gZSwis/uTe255QgMtTLA=";
+  url = "https://github.com/nixos/nixpkgs/archive/dda3dcd3fe03e991015e9a74b22d35950f264a54.tar.gz";
+  sha256 = "sha256-Ua0drDHawlzNqJnclTJGf87dBmaO/tn7iZ+TCkTRpRc=";
 })
