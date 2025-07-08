3bf3d0c3f6e7a1f4cc8cff68fd249b5099968f1d
Ruud van Asseldonk <dev@veniogames.com>
2025-07-08 17:09:04 +0200

    Update libressl to 4.1.0
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      libressl 4.0.0 -> 4.1.0

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/4206c4cb56751df534751b058295ea61357bbbaa.tar.gz";
-  sha256 = "sha256-VLFNc4egNjovYVxDGyBYTrvVCgDYgENp5bVi9fPTDYc=";
+  url = "https://github.com/nixos/nixpkgs/archive/1fd8bada0b6117e6c7eb54aad5813023eed37ccb.tar.gz";
+  sha256 = "sha256-J1kI6oAj25IG4EdVlg2hQz8NZTBNYvIS0l4wpr9KcUo=";
 })
