a46d13667235bea5dab4b061f7d9a8317c2ac773
Ruud van Asseldonk <dev@veniogames.com>
2025-08-30 10:38:36 +0200

    Update lego, publicsuffix-list, and 56 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      lego 4.25.1 -> 4.25.2
    
    Build dependencies:
    
    + Crypt-URandom                                            0.39
      abseil-cpp                 20250512.1                 -> 20250127.1
    - audit                      4.1.0
      auditable-cargo-bootstrap  1.88.0                     -> 1.89.0
      autoconf                   2.69                       -> 2.72
    - autoconf                   2.72
    + automake                                                 1.18.1
      bash                       5.3p0                      -> 5.3p3
      bash-interactive           5.3p0                      -> 5.3p3
      build                      1.2.2.post1                -> 1.3.0
      buildcatrust               0.3.0                      -> 0.4.0
      cargo                      1.88.0                     -> 1.89.0
      cargo-bootstrap            1.88.0                     -> 1.89.0
      certifi                    2025.06.15                 -> 2025.07.14
      cython                     3.0.12                     -> 3.1.2
    - db                         4.8.30
      freezegun                  1.5.1                      -> 1.5.4
      gevent                     24.11.1                    -> 25.5.1
      go                         1.24.5                     -> 1.24.6
      greenlet                   3.2.2                      -> 3.2.3
      hatch-vcs                  0.4.0                      -> 0.5.0
      kbd                        2.7.1                      -> 2.8.0
      libxml2                    2.14.4-unstable-2025-06-20 -> 2.14.5
      linux-headers              6.14.7                     -> 6.16
    - linux-pam                  1.6.1
      llvm                       20.1.6                     -> 20.1.8
      llvm-src                   20.1.6                     -> 20.1.8
      llvm-tblgen                20.1.6                     -> 20.1.8
      llvm-tblgen-src            20.1.6                     -> 20.1.8
      lxml                       5.4.0                      -> 6.0.0
      meson                      1.8.2                      -> 1.8.3
      nghttp2                    1.65.0                     -> 1.66.0
      ninja                      1.12.1                     -> 1.13.1
      nss-cacert                 3.113.1                    -> 3.114
      nss-cacert-certdata        3.113.1                    -> 3.114
      publicsuffix-list          0-unstable-2025-06-10      -> 0-unstable-2025-07-22
      pygments                   2.19.1                     -> 2.19.2
      pytest                     8.3.5                      -> 8.4.1
      pytest-asyncio             0.26.0                     -> 1.1.0
      pytest-xdist               3.6.1                      -> 3.8.0
      python3                    3.13.5                     -> 3.13.6
      python3.13-bootstrap-build 1.2.2.post1                -> 1.3.0
      re2                        2024-07-02                 -> 2025-08-05
      re2c                       4.2                        -> 4.3
      rustc                      1.88.0                     -> 1.89.0
      rustc-bootstrap            1.88.0                     -> 1.89.0
      rustc-bootstrap-wrapper    1.88.0                     -> 1.89.0
      rustc-wrapper              1.88.0                     -> 1.89.0
      setuptools                 80.7.1                     -> 80.9.0
      setuptools-scm             8.3.1                      -> 9.0.1
      tcl                        8.6.15                     -> 8.6.16
      types-psutil               7.0.0.20250401             -> 7.0.0.20250801
      types-setuptools           78.1.0.20250329            -> 80.9.0.20250801
      typing-extensions          4.13.2                     -> 4.14.1
      urllib3                    2.4.0                      -> 2.5.0
      util-linux-minimal         2.41                       -> 2.41.1
      zipp                       3.21.0                     -> 3.23.0

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/20075955deac2583bb12f07151c2df830ef346b4.tar.gz";
-  sha256 = "sha256-HMwfAJBdrr8wXAkbGhtcby1zGFvs+StOp19xNsbqdOg=";
+  url = "https://github.com/nixos/nixpkgs/archive/dfb2f12e899db4876308eba6d93455ab7da304cd.tar.gz";
+  sha256 = "sha256-1wxxznpW2CKvI9VdniaUnTT2Os6rdRJcRUf65ZK9OtE=";
 })
