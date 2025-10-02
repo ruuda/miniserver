1f49344ada8ad061d19624ab8f1eb123dd70c03c
Ruud van Asseldonk <dev@veniogames.com>
2025-10-02 23:04:21 +0200

    Update libressl to 4.1.1
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    libressl-401 branch of ruuda/nixpkgs.
    
    Runtime dependencies:
    
      libressl 4.1.0 -> 4.1.1

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/e643668fd71b949c53f8626614b21ff71a07379d.tar.gz";
-  sha256 = "sha256-NY3kSorgqE5LMm1LqNwGne3ZLMF2/ILgLpFr1fS4X3o=";
+  url = "https://github.com/ruuda/nixpkgs/archive/8f2f54801d8de51e49ee991f7df4e643f752be8a.tar.gz";
+  sha256 = "sha256-GtZksUJxnTjY74/XILbwBsmR0OqYzSeaujnr+YOxzwk=";
 })
