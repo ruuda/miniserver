097359359461a9c02fabc70e9f60f6e28a8f3cf1
Ruud van Asseldonk <dev@veniogames.com>
2026-01-18 12:10:41 +0100

    Update xgcc, lego, and 49 changes
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      lego 4.29.0 -> 4.31.0
      xgcc 14.3.0 -> 15.2.0
    
    Build dependencies:
    
      Syntax-Keyword-Try           0.29                  -> 0.30
    - Test-Simple                  1.302195
      attrs                        25.3.0                -> 25.4.0
    - boost                        1.87.0
      bootstrap-stage2-gcc-wrapper 14.3.0                -> 15.2.0
      bootstrap-stage3-gcc-wrapper 14.3.0                -> 15.2.0
      bootstrap-stage4-gcc-wrapper 14.3.0                -> 15.2.0
      brotli                       1.1.0                 -> 1.2.0
    - cargo-auditable              0.6.5
    - cargo-auditable              0.6.5-vendor
    + cargo-auditable-bootstrap                             0.7.2
    + cargo-auditable-bootstrap                             0.7.2-vendor
    + cmocka                                                1.1.8
      cython                       3.1.4                 -> 3.1.6
      freezegun                    1.5.4                 -> 1.5.5
      gcc                          14.3.0                -> 15.2.0
      gcc-wrapper                  14.3.0                -> 15.2.0
      gfortran                     14.3.0                -> 15.2.0
      gfortran-wrapper             14.3.0                -> 15.2.0
      go                           1.25.4                -> 1.25.5
      hatchling                    1.27.0                -> 1.28.0
      html5lib                     1.1                   -> 1.1-unstable-2024-02-21
      hypothesis                   6.136.9               -> 6.145.1
      libarchive                   3.8.2                 -> 3.8.4
      libxslt                      1.1.43                -> 1.1.45
      llvm                         21.1.2                -> 21.1.7
      llvm-src                     21.1.2                -> 21.1.7
      llvm-tblgen-src              21.1.2                -> 21.1.7
      nghttp3                      1.12.0                -> 1.13.1
      ngtcp2                       1.17.0                -> 1.18.0
      ninja                        1.13.1                -> 1.13.2
      nss-cacert                   3.115                 -> 3.117
      nss-cacert-certdata          3.115                 -> 3.117
      numpy                        2.3.4                 -> 2.3.5
      perl                         5.40.0                -> 5.42.0
      psutil                       7.1.2                 -> 7.1.3
      publicsuffix-list            0-unstable-2025-10-08 -> 0-unstable-2025-11-14
    - pystemmer                    2.2.0.1
      pytest-asyncio               1.2.0                 -> 1.3.0
    + pytest-expect                                         1.1.0
      python3                      3.13.9                -> 3.13.11
      re2c                         4.3                   -> 4.3.1
      spdlog                       1.15.3                -> 1.16.0
      sqlite                       3.50.4                -> 3.51.1
      trove-classifiers            2025.9.11.17          -> 2025.11.14.15
    + u-msgpack-python                                      2.8.0
      urllib3                      2.5.0                 -> 2.6.0
      zope-event                   5.0                   -> 6.0
      zope-interface               7.2                   -> 8.0.1

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/3e2499d5539c16d0d173ba53552a4ff8547f4539.tar.gz";
-  sha256 = "sha256-QEhk0eXgyIqTpJ/ehZKg9IKS7EtlWxF3N7DXy42zPfU=";
+  url = "https://github.com/nixos/nixpkgs/archive/e4bae1bd10c9c57b2cf517953ab70060a828ee6f.tar.gz";
+  sha256 = "sha256-Kell/SpJYVkHWMvnhqJz/8DqQg2b6PguxVWOuadbHCc=";
 })
