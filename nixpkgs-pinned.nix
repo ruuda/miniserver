721240e932ef46ecd1cc2ce6865e4707483ce7f2
Ruud van Asseldonk <dev@veniogames.com>
2026-02-20 20:27:08 +0100

    Update nginx to 1.29.5, and one more change
    This updates the pinned Nixpkgs snapshot to the latest commit in the
    nixos-unstable branch of nixos/nixpkgs.
    
    Runtime dependencies:
    
      nginx 1.29.4 -> 1.29.5
    
    Build dependencies:
    
    + nix-prefetch-git      26.05

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs/archive/c5296fdd05cfa2c187990dd909864da9658df755.tar.gz";
-  sha256 = "sha256-oMmN1lVQU0F0W2k6OI3bgdzp2YOHWYUAw79qzDSjenU=";
+  url = "https://github.com/nixos/nixpkgs/archive/0182a361324364ae3f436a63005877674cf45efb.tar.gz";
+  sha256 = "sha256-0NBlEBKkN3lufyvFegY4TYv5mCNHbi5OmBDrzihbBMQ=";
 })
