fdfaa8ceea936b54ee23ff93bac08c3b96ab5555
Ruud van Asseldonk <dev@ruuda.nl>
2026-06-04 14:26:46 +0200

    Make entire Outline share available, add /bin/sh

--- default.nix
+++ default.nix
@@ -343,7 +343,16 @@ let
       ln -s ${pkg}/bin/outline-server $out/usr/bin/outline-server
 
       # Outline binaries try to read /build, ensure it exists inside the chroot.
+
       ln -s ${pkg}/share/outline/build $out/build
+
+      # Outline binaries try to read /build. We make it available in
+      # at /share/outline, so the working directory must point there. This is
+      # also required for the migrations to work.
+      ln -s ${pkg}/share/outline $out/usr/share
+
+      # Migrations try to execute `/bin/sh`. `/bin` points to `/usr/bin`.
+      ln -s ${pkgs.bash}/bin/bash $out/usr/bin/sh
       '';
   };
 
