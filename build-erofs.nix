50c37e3d7cf016e8a6838817fb92caaf9cb8af8b
Ruud van Asseldonk <dev@ruuda.nl>
2026-06-23 14:43:32 +0200

    Begin splitting packages into directories
    This breaks the top-level default.nix. Nginx is ported, it now lives in
    packages/nginx. I will migrate the remaining packages, and then adjust
    the deployment and update tooling to deal with the new structure.

---
+++ build-erofs.nix
@@ -0,0 +1,168 @@
+# Miniserver -- EROFS webserver packages for Flatcar Linux.
+# Copyright 2026 Ruud van Asseldonk
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3. A copy
+# of the License is available in the root of the repository.
+
+{ pkgs }: rec {
+  # Put together the filesystem by copying from and symlinking to the Nix store.
+  # Later we build this into an image with `mkfs.erofs`. Symlinks are included
+  # verbatim, so we place symlinks into `/nix/store`, not into `$out`. When the
+  # resulting image is mounted, they link to the right places.
+  buildImageDir = {
+    # The main package to build the filesystem for.
+    pkg,
+
+    # Additional packages to include in the closure, for example a shell if the
+    # program in the main package tries to exec a shell.
+    extraPackages,
+
+    # Commands to run to modify the filesystem at $out, for example to add
+    # additional symlinks.
+    extraBuildCommand,
+
+    # Whether to reduce the filesystem size by deleting unneeded files such as
+    # locales from the glibc store paths. This might break applications that
+    # do need those files.
+    minimize,
+  }:
+    let
+      inputs = [ pkg ] ++ extraPackages;
+    in
+      pkgs.stdenv.mkDerivation {
+        name = "${pkg.name}-fs";
+        buildInputs = inputs;
+        inherit minimize;
+        buildCommand = ''
+          # Although we only need /nix/store and /usr/bin, we need to create the
+          # other directories too so systemd can mount the API virtual filesystems
+          # there, when the image is used. For /var, for systemd-nspawn only /var is
+          # sufficient, but in a unit with PrivateTmp=true, we also need /var/tmp,
+          # because systemd mounts a tmpfs there. /run is not needed by the systemd
+          # unit, but it is required by systemd-nspawn, so we add it too.
+          mkdir -p $out/dev
+          mkdir -p $out/etc/ssl/certs
+          mkdir -p $out/nix/store
+          mkdir -p $out/proc
+          mkdir -p $out/run
+          mkdir -p $out/sys
+          mkdir -p $out/tmp
+          mkdir -p $out/usr/bin
+          mkdir -p $out/usr/share/ca-certificates
+          mkdir -p $out/var/log/journal
+          mkdir -p $out/var/tmp
+
+          touch $out/etc/resolv.conf
+          touch $out/etc/passwd
+          touch $out/etc/group
+          ln -s /usr/bin $out/bin
+
+          ${extraBuildCommand}
+
+          closureInfo=${pkgs.closureInfo { rootPaths = inputs; }}
+          for file in $(cat $closureInfo/store-paths); do
+            echo "copying $file"
+            cp --archive $file $out/nix/store
+          done
+
+          # The steps below minimize the filesystem size. If that is not
+          # requested, we are done here.
+          if [ -z "$minimize" ]; then
+            exit 1
+          fi
+
+          # Slim down the glibc installation by removing unused locale data. We do
+          # this here, and not in the glibc package, to avoid rebuilding everything
+          # that depends on glibc. We need to make the containing directories
+          # writable to be able to remove files from them.
+          cd $out${pkgs.glibc}/share
+          chmod --recursive +w .
+
+          rm -fr locale
+          mv i18n/locales/C i18n/locales_C
+          rm i18n/locales/*
+          mv i18n/locales_C i18n/locales/C
+
+          # Delete all the charmaps, they consume a lot of space, and we do not use
+          # them.
+          rm i18n/charmaps/*.gz
+
+          chmod --recursive -w .
+
+          # Delete the gconv shared objects related to locales, the programs we run
+          # do not use iconv.
+          cd $out${pkgs.glibc}/lib/gconv
+          chmod +w .
+          rm *.so
+          chmod -w .
+
+          # Also for libidn2, we don't need those locales, we are only running Nginx.
+          cd $out${pkgs.libidn2.out}/share/locale
+          chmod --recursive +w .
+          rm -r *
+          chmod -w .
+        '';
+      };
+
+  buildImage = {
+    label,
+    pkg,
+    extraBuildCommand,
+    extraPackages,
+    minimize,
+  }:
+    # EROFS filesystem labels can be at most 15 characters long.
+    assert builtins.stringLength label <= 15;
+    pkgs.stdenv.mkDerivation rec {
+      name = "${pkg.name}-image";
+      imageName = "${pkg.name}.img";
+      imageDir = buildImageDir { inherit pkg extraPackages extraBuildCommand minimize; };
+
+      nativeBuildInputs = [ pkgs.cryptsetup pkgs.erofs-utils pkgs.jq pkgs.python3 ];
+      buildInputs = [ imageDir ];
+
+      # Make Nix dump the details of the closure of the packages as part of the
+      # NIX_ATTRS_JSON_FILE.
+      __structuredAttrs = true;
+      exportReferencesGraph.pkgClosure = [ pkg ] ++ extraPackages;
+
+      # There is no significant size difference between level=6 and level=12,
+      # though there is a significant difference in compression time. So we opt
+      # for the faster mode.
+      buildCommand =
+        ''
+        mkdir -p $out
+        mkfs.erofs $out/${imageName} ${imageDir} -L ${label} -zlz4hc,level=6
+        veritysetup format \
+          --uuid=$(python3 ${./deterministic_uuid.py} uuid $out/${imageName}) \
+          --salt=$(python3 ${./deterministic_uuid.py} salt $out/${imageName}) \
+          --root-hash-file=$out/${imageName}.roothash \
+          $out/${imageName} $out/${imageName}.verity
+
+        # Include closure information about the packages stored in the filesystem
+        # image. We can use this to detect changes between version bumps, and to
+        # build an SBOM.
+        jq .pkgClosure $NIX_ATTRS_JSON_FILE > $out/packages.json
+        '';
+    };
+
+  buildImageManifest = {
+    name,
+    pkg,
+    extraBuildCommand,
+    extraPackages ? [],
+    minimize ? false,
+  }:
+    let
+      image = buildImage {
+        label = name;
+        inherit pkg extraBuildCommand extraPackages minimize;
+      };
+    in
+      pkgs.stdenv.mkDerivation {
+        name = "${pkg.name}-image.json";
+        nativeBuildInputs = [ pkgs.python3 ];
+        buildCommand = "python3 ${./build_manifest.py} ${name} ${image} > $out";
+      };
+}
--- default.nix
+++ default.nix
@@ -64,106 +64,6 @@ let
     uringSupport = true;
   });
 
-  lightNginx = pkgs.nginxMainline.override {
-    # Remove dependency on libgd; It brings in a lot of transitive dependencies
-    # that we don't need (fontconfig, image codecs, etc.). Also disable other
-    # unnecessary dependencies.
-    gd = null;
-    withStream = false;
-    withMail = false;
-    modules = [];
-
-    # Build Nginx against LibreSSL, rather than OpenSSL. This reduces the size
-    # of the image, as we don't have to include both OpenSSL and LibreSSL. But
-    # more importantly, I trust LibreSSL more than I trust OpenSSL.
-    openssl = libressl;
-  };
-
-  ngxBrotli = pkgs.fetchFromGitHub {
-    owner = "google";
-    repo = "ngx_brotli";
-    sha256 = "sha256-ks5Ae9gCscEX8TkqK3LGiRl2twUt+chGfkrRhMXS7uc=";
-    rev = "6e975bcb015f62e1f303054897783355e2a877dc";
-    fetchSubmodules = true;
-  };
-
-  defaultNginxConfig = pkgs.writeText "nginx.conf" ''
-    # Don't daemonize. This makes it easier to run under systemd, especially
-    # with RootImage=, as there are no pidfiles to juggle around, and no
-    # directories that we need to create or mount for that. It also simplifies
-    # stopping, because systemd sends the right signal to the process directly.
-    # See also https://nginx.org/en/docs/faq/daemon_master_process_off.html.
-    daemon off;
-    error_log /var/log/nginx/error.log;
-
-    worker_processes auto;
-
-    events {
-      worker_connections 1024;
-    }
-
-    http {
-      # Same log format as the default "combined" format, but including the host.
-      log_format vhosts '$host: $remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"';
-      access_log /var/log/nginx/access.log vhosts;
-
-      include /etc/nginx/sites-enabled/*;
-    }
-  '';
-
-  customNginx = lightNginx.overrideDerivation (oldAttrs: {
-    # Override the light nginx package to cut down on the dependencies further.
-    # I also want to get rid of geoip and all of the xml stuff, but the package
-    # offers no options for that. Furthermore, enable the ngx_brotli module.
-    configureFlags = [
-      "--with-http_ssl_module"
-      "--with-http_v2_module"
-      "--with-http_v3_module"
-      "--with-http_auth_request_module"
-      "--with-http_gzip_static_module"
-      "--with-threads"
-      "--with-pcre-jit"
-      "--with-ipv6"
-      "--add-module=ngx_brotli"
-      # If the group is not set explicitly, the configure script will first look
-      # for a "nobody" group in /etc/group and then fall back to "nogroup". To
-      # keep the build reproducible and independent of /etc/group on the host
-      # system, set the group explicitly.
-      "--group=nogroup"
-      # Configure default paths. They default to /nix/store/...-nginx/logs/*,
-      # but that is inconvenient because we would need to update the systemd
-      # unit every time (or generate it with Nix, but it is delivered outside of
-      # the image, so that is not really an option). We also provide a
-      # customized default config file that does not write logs to these paths.
-      "--conf-path=${defaultNginxConfig}"
-      # We don't run in daemon mode; there is no need to write a pidfile.
-      "--pid-path=/dev/null"
-      "--error-log-path=stderr" #/var/log/nginx/error.log"
-      #"--http-log-path=/var/log/nginx/access.log"
-
-      # Nginx writes some times writes things to temporary files, by default in
-      # /nix/store/...nginx/client_body_temp, but that fails on the immutable
-      # file system, so point it to /tmp (which is a ramdisk), and private to
-      # Nginx anyway because we use PrivateTmp= in the systemd unit. Note that
-      # the subdirectories do not actually exist.
-      "--http-client-body-temp-path=/tmp/client_body_temp"
-      "--http-fastcgi-temp-path=/tmp/fastcgi_temp"
-      "--http-proxy-temp-path=/tmp/proxy_temp"
-      "--http-scgi-temp-path=/tmp/scgi_temp"
-      "--http-uwsgi-temp-path=/tmp/uwsgi_temp"
-    ];
-
-    # The nginx binary embeds its configure command line. If we would pass the
-    # ngx_brotli module store path directly to --add-module, the store path
-    # would therefore end up in the binary. That triggers Nix to detect the
-    # ngx_brotli source as a runtime dependency, even though it is not. Work
-    # around this issue by creating a symlink to the store path in the build
-    # directory. Then the configure flags no longer include the path itself.
-    preConfigure = oldAttrs.preConfigure + ''
-      ln -s ${ngxBrotli} ngx_brotli
-    '';
-  });
-
   # Put together the filesystem by copying from and symlinking to the Nix store.
   # Later we build this into an image with `mkfs.erofs`. Symlinks are included
   # verbatim, so we place symlinks into `/nix/store`, not into `$out`. When the
@@ -272,19 +172,6 @@ let
       # image. We can use this to detect changes between version bumps, and to
       # build an SBOM.
       jq .pkgClosure $NIX_ATTRS_JSON_FILE > $out/packages.json
-      '';
-  };
-
-  imageNginx = buildImage rec {
-    label = "miniserver-ngx";
-    pkg = customNginx;
-    extraBuildCommand =
-      ''
-      mkdir -p $out/etc/nginx
-      mkdir -p $out/var/lib/lego/certificates
-      mkdir -p $out/var/log/nginx
-      mkdir -p $out/var/www
-      ln -s ${pkg}/bin/nginx $out/usr/bin/nginx
       '';
   };
 
@@ -413,7 +300,6 @@ in
       python3 ${./build_manifest.py} \
         forgejo=${imageForgejo} \
         lego=${imageLego} \
-        nginx=${imageNginx} \
         nsd=${imageNsd} \
         outline=${imageOutline} \
         postgres=${imagePostgres} \
