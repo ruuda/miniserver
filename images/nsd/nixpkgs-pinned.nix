903d00d9f86dea9976ff61ecef49dfeafa768f0e
Ruud van Asseldonk <dev@ruuda.nl>
2026-06-25 22:16:38 +0200

    Update nsd: nsd 4.14.3, openssl, and 33 changes
    This updates the pinned Nixpkgs snapshot for nsd to the latest commit in
    the nsd-4.14.3 branch of ruuda/nixpkgs.
    
    Image size: 17.23 MB -> 17.38 MB (+0.9%)
    
    Runtime dependencies:
    
    + fstrm                0.6.1
      libressl   4.3.1  -> 4.2.1
      nsd        4.12.0 -> 4.14.3
      openssl    3.6.1  -> 3.6.2
    + protobuf-c           1.5.2
    - xgcc       15.2.0
    
    Build dependencies:
    
    + abseil-cpp                                           20260107.1
    + asciidoc                                             10.2.1
      bootstrap-coreutils-musl             9.9          -> 9.10
      bootstrap-coreutils-musl-get-version 9.9          -> 9.10
      build                                1.4.2        -> 1.4.4
      coreutils                            9.10         -> 9.11
      coreutils-static                     9.9          -> 9.10
      coreutils-static-get-version         9.9          -> 9.10
      curl                                 8.19.0       -> 8.20.0
    + docbook-xml                                          4.5
    + docbook-xsl-nons                                     1.79.2
    + docbook-xsl-ns                                       1.79.2
      expat                                2.7.5        -> 2.8.0
      file                                 5.45         -> 5.47
    + fstrm                                                0.6.1
      gnum4                                1.4.20       -> 1.4.21
    - gnum4                                1.4.21
      gnum4-get-version                    1.4.20       -> 1.4.21
    + gtest                                                1.17.0
      libarchive                           3.8.6        -> 3.8.7
      libcap-ng                            0.9.2        -> 0.9.3
    + libevent                                             2.1.12
    + libressl                                             4.2.1
      libxml2                              2.15.1       -> 2.15.2
    + libxslt                                              1.1.45
      nghttp2                              1.68.1       -> 1.69.0
    + ninja                                                1.13.2
      nss-cacert                           3.121        -> 3.123
      openssl                              3.6.1        -> 3.6.2
      packaging                            25.0         -> 26.1
      pathspec                             0.12.1       -> 1.0.4
    + protobuf                                             33.6
    + protobuf                                             34.1
    + protobuf-c                                           1.5.2
      pytest                               9.0.2        -> 9.0.3
    + pytest-asyncio                                       1.3.0
    + pytest-mock                                          3.15.1
      python                               3.14.2       -> 3.14.4
      python-get-version                   3.14.2       -> 3.14.4
      python3.13-bootstrap-build           1.4.2        -> 1.4.4
      python3.13-bootstrap-packaging       25.0         -> 26.1
    + re2c                                                 4.5.1
      trove-classifiers                    2026.1.14.14 -> 2026.4.28.13
      tzdata                               2026a        -> 2026b
      xz                                   5.4.7        -> 5.8.3
    - xz                                   5.8.2
    - xz                                   5.8.3
      xz-get-version                       5.4.7        -> 5.8.3
      zlib                                 1.3.1        -> 1.3.2
    - zlib                                 1.3.2

--- images/nsd/nixpkgs-pinned.nix
+++ images/nsd/nixpkgs-pinned.nix
@@ -1,10 +1,10 @@
 rec {
   owner = "ruuda";
   repo = "nixpkgs";
-  commit = "69d860e0e0e115deecf32e235e279cca0bb67545";
-  commit_date = "2026-05-15T09:23:15Z";
+  commit = "904dd9839a463bf89bc6c97a269da3a05326b74d";
+  commit_date = "2026-06-25T20:06:58Z";
   tarball = fetchTarball {
     url = "https://github.com/${owner}/${repo}/archive/${commit}.tar.gz";
-    sha256 = "sha256-Ge9fB7iLOThZaiUEG6GoUPjF3JB7OFY/7BLv8WG4hTo=";
+    sha256 = "sha256-FuBrxQt8QIIk7RTlsfju5oKLTOp+LvHJYrzTuDU3kFA=";
   };
 }
